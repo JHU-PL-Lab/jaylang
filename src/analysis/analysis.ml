5a2a3c5d61e7277ea57f70f8ee691c0315faf64f
Zachary Palmer <zep_git@bahj.com>
2015-09-10 14:20:35 -0400

    Added skeletal CBA graph code.

--- src/analysis/analysis.ml
+++ src/analysis/analysis.ml
@@ -3,14 +3,159 @@
   in the choice of context stack.
 *)
 
+open Batteries;;
+
 open Analysis_context_stack;;
+open Ast;;
+open Ast_pretty;;
+open Cba_graph;;
 
 let logger = Logger_utils.make_logger "Analysis";;
+
+module Pattern_ord =
+struct
+  type t = pattern
+  let compare = compare_pattern
+end;;
+
+module Pattern_set = Set.Make(Pattern_ord);;
+
+let pp_pattern_set pats =
+  String_utils.concat_sep_delim "{" "}" ", " @@ Enum.map pretty_pattern @@
+    Pattern_set.enum pats
+;;
+
+module type Analysis_sig =
+sig
+  type cba_analysis
+  
+  val create_analysis : expr -> cba_analysis
+  
+  val values_of_variable : var -> cba_analysis -> Abs_value_set.t
+end;;
 
 (**
   A functor which constructs a CBA analysis module.
 *)
 module Make(C : Context_stack) =
 struct
+  type pds_continuation =
+    | Lookup_var of var * Pattern_set.t * Pattern_set.t
+    | Project of ident * Pattern_set.t * Pattern_set.t
+    | Jump of var * annotated_clause * C.t * Pattern_set.t * Pattern_set.t
+    | Deref of Pattern_set.t * Pattern_set.t
+    | Capture
+    | Continuation_value of abstract_value
+    | Alias_huh
+    [@@deriving ord]
+  ;;
+
+  module Pds_continuation_ord =
+  struct
+    type t = pds_continuation
+    let compare = compare_pds_continuation
+  end;;
+
+  let pp_pds_continuation = function
+    | Lookup_var(x,patsp,patsn) ->
+      Printf.sprintf "%s(%s/%s)"
+        (pretty_var x) (pp_pattern_set patsp) (pp_pattern_set patsn)
+    | Project(i,patsp,patsn) ->
+      Printf.sprintf ".%s(%s/%s)"
+        (pretty_ident i) (pp_pattern_set patsp) (pp_pattern_set patsn)
+    | Jump(x,acl,ctx,patsp,patsn) ->
+      Printf.sprintf "Jump(%s,%s,%s,%s,%s)"
+        (pretty_var x) (pp_annotated_clause acl) (C.pretty ctx)
+          (pp_pattern_set patsp) (pp_pattern_set patsn)
+    | Deref(patsp,patsn) ->
+      Printf.sprintf "!(%s,%s)" (pp_pattern_set patsp) (pp_pattern_set patsn)
+    | Capture -> "Capture"
+    | Continuation_value v -> pp_abstract_value v
+    | Alias_huh -> "Alias?"
+  ;;
+
+  type pds_state =
+    Pds_state of annotated_clause * C.t * Pattern_set.t * Pattern_set.t
+    [@@deriving ord]
+  ;;
+
+  let pp_pds_state (Pds_state(acl,ctx,patsp,patsn)) =
+    Printf.sprintf "(%s,%s,%s,%s)"
+      (pp_annotated_clause acl) (C.pretty ctx) (pp_pattern_set patsp) (pp_pattern_set patsn)
+  ;;
+
+  module Pds_state_ord =
+  struct
+    type t = pds_state
+    let compare = compare_pds_state
+  end;;
   
+  module Cba_pds_reachability = Pds_reachability.Make(
+    struct
+      type state = pds_state
+      type stack_element = pds_continuation
+      module State_ord = Pds_state_ord
+      module Stack_element_ord = Pds_continuation_ord
+      let pp_state = pp_pds_state
+      let pp_stack_element = pp_pds_continuation
+    end
+  );;
+
+  (*
+    Here, we're creating the analysis data type in an internal module with a
+    signature that specifically abstracts its internals.  This helps us
+    guarantee that the analysis is not directly manipulated outside of this
+    module; that encapsulation helps us keep the CBA graph and the reachability
+    graph in sync.
+  *)
+  module type Analysis_struct_sig =
+  sig
+    type cba_analysis
+    val empty_analysis : cba_analysis
+    val add_edge : cba_edge -> cba_analysis -> cba_analysis
+  end;;
+
+  module Analysis_struct : Analysis_struct_sig =
+  struct
+    type cba_analysis =
+      Cba_analysis of cba_graph * Cba_pds_reachability.reachability_analysis
+    ;;
+    let empty_analysis =
+      Cba_analysis(Cba_graph.empty, Cba_pds_reachability.empty)
+    ;;
+    let add_edge edge analysis =
+      raise @@ Utils.Not_yet_implemented "add_edge for analysis structure"
+    ;;
+  end;;
+
+  include Analysis_struct;;
+
+  let create_analysis e =
+    let Abs_expr(cls) = lift_expr e in
+    (* Put the annotated clauses together. *)
+    let acls =
+      List.enum cls
+      |> Enum.map (fun x -> Unannotated_clause x)
+      |> Enum.append (Enum.singleton Start_clause)
+      |> flip Enum.append (Enum.singleton End_clause)
+    in
+    (* For each pair, produce a CBA edge. *)
+    let rec mk_edges acls' =
+      match Enum.get acls' with
+      | None -> []
+      | Some acl1 ->
+        match Enum.peek acls' with
+        | None -> []
+        | Some acl2 ->
+          Cba_edge(acl1,acl2) :: mk_edges acls'
+    in
+    let edges = mk_edges acls in
+    List.fold_left (flip add_edge) empty_analysis edges
+  ;;
+
+  let values_of_variable x analysis =
+    raise @@ Utils.Not_yet_implemented "values_of_variable"
+  ;;
+
+
 end;;
---
+++ src/analysis/cba_graph.ml
@@ -0,0 +1,166 @@
+(**
+  A module defining data structures and basic operations to form a CBA graph.
+*)
+
+open Batteries;;
+
+open Ast;;
+open Ast_pretty;;
+
+(** A type to express abstract values. *)
+type abstract_value =
+  | Abs_value_record of record_value
+  | Abs_value_function of abstract_function_value
+  | Abs_value_ref of ref_value
+  [@@deriving eq, ord]
+
+and abstract_function_value =
+  | Abs_function_value of var * abstract_expr
+  [@@deriving eq, ord]
+
+(** A type to represent the bodies of abstract clauses. *)
+and abstract_clause_body =
+  | Abs_value_body of abstract_value
+  | Abs_var_body of var
+  | Abs_appl_body of var * var
+  | Abs_conditional_body of
+      var * pattern * abstract_function_value * abstract_function_value
+  | Abs_projection_body of var * ident
+  | Abs_deref_body of var
+  | Abs_update_body of var * var
+  [@@deriving eq, ord]
+
+(** A type to represent abstract clauses. *)
+and abstract_clause =
+  | Abs_clause of var * abstract_clause_body
+  [@@deriving eq, ord]
+
+(** A type to represent abstract expressions. *)
+and abstract_expr = Abs_expr of abstract_clause list [@@deriving eq, ord]
+;;
+
+let rec pp_abstract_function_value (Abs_function_value(x,e)) =
+  Printf.sprintf "%s -> { %s }" (pretty_var x) (pp_abstract_expr e)
+
+and pp_abstract_value v =
+  match v with
+  | Abs_value_record r -> pretty_record_value r
+  | Abs_value_function f -> pp_abstract_function_value f
+  | Abs_value_ref r -> pretty_ref_value r
+
+and pp_abstract_clause_body b =
+  match b with
+  | Abs_var_body(x) -> pretty_var x
+  | Abs_value_body(v) -> pp_abstract_value v
+  | Abs_appl_body(x1,x2) -> pretty_var x1 ^ " " ^ pretty_var x2
+  | Abs_conditional_body(x,p,f1,f2) ->
+    pretty_var x ^ " ~ " ^ pretty_pattern p ^ " ? " ^
+    pp_abstract_function_value f1 ^ " : " ^ pp_abstract_function_value f2
+  | Abs_projection_body(x,i) -> pretty_var x ^ "." ^ pretty_ident i
+  | Abs_deref_body(x) -> "!" ^ pretty_var x
+  | Abs_update_body(x1,x2) -> pretty_var x1 ^ " <- " ^ pretty_var x2
+
+and pp_abstract_clause (Abs_clause(x,b)) =
+  Printf.sprintf "%s = %s" (pretty_var x) (pp_abstract_clause_body b)
+  
+and pp_abstract_expr (Abs_expr(cls)) =
+  String_utils.concat_sep "; " @@ Enum.map pp_abstract_clause @@ List.enum cls
+;;
+
+module Abs_value_order =
+struct
+  type t = abstract_value
+  let compare = compare_abstract_value
+end;;
+
+module Abs_value_set = Set.Make(Abs_value_order);;
+
+type annotated_clause =
+  | Unannotated_clause of abstract_clause
+  | Enter_clause of var * var * abstract_clause
+  | Exit_clause of var * var * abstract_clause
+  | Start_clause
+  | End_clause
+  [@@deriving ord]
+;;
+
+let pp_annotated_clause acl =
+  match acl with
+  | Unannotated_clause(cl) -> pp_abstract_clause cl
+  | Enter_clause(x,x',cl) -> Printf.sprintf "%s=%s@%s+"
+      (pretty_var x) (pretty_var x') (pp_abstract_clause cl)
+  | Exit_clause(x,x',cl) -> Printf.sprintf "%s=%s@%s+"
+      (pretty_var x) (pretty_var x') (pp_abstract_clause cl)
+  | Start_clause -> "Start"
+  | End_clause -> "End"
+;;
+
+type cba_edge =
+  | Cba_edge of annotated_clause * annotated_clause
+  [@@deriving ord]
+;;
+
+module Cba_edge_ord =
+struct
+  type t = cba_edge
+  let compare = compare_cba_edge
+end;;
+
+(*
+  Creating the graph data type inside of a module.  This allows us to keep the
+  graph data type intentionally abstract, thus permitting safe indexing and
+  other helpful features.
+*)
+module type Graph_sig =
+sig
+  type cba_graph
+  
+  val empty : cba_graph
+  
+  val add_edge : cba_edge -> cba_graph -> cba_graph
+  
+  val edges_of : cba_graph -> cba_edge Enum.t
+end;;
+
+module Graph_impl : Graph_sig =
+struct
+  module Cba_edge_set = Set.Make(Cba_edge_ord);;
+  
+  type cba_graph = Graph of Cba_edge_set.t;;
+
+  let empty = Graph(Cba_edge_set.empty);;
+
+  let add_edge edge (Graph(s)) = Graph(Cba_edge_set.add edge s);;
+
+  let edges_of (Graph(s)) = Cba_edge_set.enum s;;
+end;;
+
+include Graph_impl;;
+
+let rec lift_expr (Expr(cls)) =
+  Abs_expr(List.map lift_clause cls)
+
+and lift_clause (Clause(x,b)) =
+  Abs_clause(x, lift_clause_body b)
+
+and lift_clause_body b =
+  match b with
+  | Value_body v -> Abs_value_body(lift_value v)
+  | Var_body x -> Abs_var_body x
+  | Appl_body(x,x') -> Abs_appl_body(x,x')
+  | Conditional_body(x,p,f1,f2) ->
+    Abs_conditional_body(x,p,lift_function_value f1,lift_function_value f2)
+  | Projection_body(x,i) -> Abs_projection_body(x,i)
+  | Deref_body(x) -> Abs_deref_body(x)
+  | Update_body(x,x') -> Abs_update_body(x,x')
+
+and lift_value v =
+  match v with
+  | Value_record r -> Abs_value_record r
+  | Value_function f -> Abs_value_function(lift_function_value f)
+  | Value_ref r -> Abs_value_ref r
+
+and lift_function_value (Function_value(x,e)) =
+  Abs_function_value(x, lift_expr e)
+
+;;
