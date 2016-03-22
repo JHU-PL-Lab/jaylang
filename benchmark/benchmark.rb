3f701cbb0f3b37e5fb136488de299a1e53ac7bc0
Leandro Facchinetti <me@leafac.com>
2016-03-22 14:53:55 -0400

    Fix baseline benchmark

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -3,7 +3,7 @@
 #
 # This script times the execution and reports numbers.
 
-TEST_ON_KS = (0..1)
+TEST_ON_KS = [0]
 
 RULER = "==========================================="
 CURRENT_DIRECTORY = File.expand_path("..", __FILE__)
@@ -12,7 +12,7 @@ require "benchmark"
 
 TEST_ON_KS.each do |k|
   Dir["#{CURRENT_DIRECTORY}/../benchmark-sources/*.code"].each do |file|
-    command_line = "#{CURRENT_DIRECTORY}/../toploop.native --select-context-stack=#{k}ddpa --disable-evaluation --disable-inconsistency-check --report-sizes < #{file}"
+    command_line = "#{CURRENT_DIRECTORY}/../toploop.native --select-context-stack=#{k}ddpa --analyze-variables=all --disable-evaluation --disable-inconsistency-check --report-sizes < #{file}"
     puts RULER
     puts "Test: `#{File.basename(file, ".*")}'."
     puts "k: `#{k}'."
