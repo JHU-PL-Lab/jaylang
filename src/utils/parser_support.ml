d71878f72d47173edc16900b4564ddada30eb8c0
Zachary Palmer <zep_git@bahj.com>
2016-06-01 11:03:10 -0400

    Added missing parser utility function.

--- src/utils/parser_support.ml
+++ src/utils/parser_support.ml
@@ -1,3 +1,5 @@
+open Lexing;;
+open Source_origin;;
 open Uid;;
 
 let uid_table = ref @@ [];;
@@ -11,4 +13,20 @@ let put (uid : uid) file_region =
 let get_all () =
   List.fold_right
     (fun (key,value) acc -> Uid_map.add key value acc) !uid_table Uid_map.empty
+;;
+
+let next_uid startpos endpos =
+  let uid : uid = Uid.next_uid () in
+  let start = { file_pos_lineno = startpos.pos_lnum
+              ; file_pos_colno = startpos.pos_bol
+              } in
+  let stop = { file_pos_lineno = endpos.pos_lnum
+             ; file_pos_colno = endpos.pos_bol
+             } in
+  let region = { file_region_filename = startpos.pos_fname
+               ; file_region_start = start
+               ; file_region_end = stop
+               } in
+  put uid region;
+  uid
 ;;
--- src/utils/parser_support.mli
+++ src/utils/parser_support.mli
@@ -7,11 +7,17 @@
 open Source_origin;;
 open Uid;;
 
-(** Resets the contents of the global source origin mapping. *)
+(** Resets the contents of the global file region mapping. *)
 val reset : unit -> unit
 
-(** Puts a new entry into the global source origin mapping. *)
+(** Puts a new entry into the global file region mapping. *)
 val put : uid -> file_region -> unit
 
-(** Retrieves all source origin mappings as an independent dictionary. *)
+(** Retrieves all file region mappings as an independent dictionary. *)
 val get_all : unit -> file_region Uid_map.t
+
+(** A routine the parser can use to store position information about the AST as
+    it is parsed.  This function accepts the start and end location of a
+    Menhir production.  It stores these locations in the global file region
+    mapping under a fresh UID and then returns the UID it used. *)
+val next_uid : Lexing.position -> Lexing.position -> uid
