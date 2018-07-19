38b70dad243ab22b537642bf338b6d88b269cb53
Leandro Facchinetti <me@leafac.com>
2018-07-19 15:50:48 -0400

    Add support for multiple repetitions

--- benchmark/run.rkt
+++ benchmark/run.rkt
@@ -7,29 +7,32 @@
 
   ;; PARAMETERS
 
+  (define repetitions (make-parameter 5))
+
   (define timeout (make-parameter "30m"))
 
   (define path/results (make-parameter "results"))
 
   (define path/ddpa/cases (make-parameter "cases/odefa"))
-  (define path/ddpa/analysis (make-parameter "../ddpa/core_toploop_main.native"))
-
-  (define path/little-store/cases (make-parameter "cases/odefa"))
-  (define path/little-store/analysis (make-parameter "../little-store/core_toploop_main.native"))
+  (define path/ddpa/analysis (make-parameter "../toploop_main.native"))
 
   (define path/p4f/cases (make-parameter "cases/scheme"))
-  (define path/p4f/sbt (make-parameter "../sbt/bin/sbt"))
-  (define path/p4f/working-directory (make-parameter "../p4f-prototype"))
+  (define path/p4f/analysis (make-parameter "../../p4f/"))
 
   (command-line
    #:once-each
+   ["--repetitions"
+    a-repetitions
+    ((~a "How many times to run the benchmark (default: “" (repetitions) "”)"))
+    (repetitions (or (string->number a-repetitions)
+                     (raise-user-error (~a "Given ‘repetitions’ not a number: " a-repetitions))))]
    ["--timeout"
     a-timeout
     ((~a "Timeout (as understood by “timeout(1)”) (default: “" (timeout) "”)"))
     (timeout a-timeout)]
-   ["--path-benchmark-results"
+   ["--path-results"
     path
-    ((~a "Path in which to store benchmark results (default: “" (path/results) "”)"))
+    ((~a "Path in which to store results (default: “" (path/results) "”)"))
     (path/results path)]
    ["--path-ddpa-cases"
     path
@@ -39,26 +42,14 @@
     path
     ((~a "Path to DDPA program (default: “" (path/ddpa/analysis) "”)"))
     (path/ddpa/analysis path)]
-   ["--path-little-store-cases"
-    path
-    ((~a "Path to Little Store test cases (default: “" (path/little-store/cases) "”)"))
-    (path/little-store/cases path)]
-   ["--path-little-store-analysis"
-    path
-    ((~a "Path to Little Store program (default: “" (path/little-store/analysis) "”)"))
-    (path/little-store/analysis path)]
    ["--path-p4f-cases"
     path
     ((~a "Path to P4F test cases (default: “" (path/p4f/cases) "”)"))
     (path/p4f/cases path)]
-   ["--path-p4f-sbt"
+   ["--path-p4f-analysis"
     path
-    ((~a "Path to P4F’s instance of SBT (default: “" (path/p4f/sbt) "”)"))
-    (path/p4f/sbt path)]
-   ["--path-p4f-working-directory"
-    path
-    ((~a "Path to P4F’s working directory (default: “" (path/p4f/working-directory) "”)"))
-    (path/p4f/working-directory path)])
+    ((~a "Path to P4F’s working directory (default: “" (path/p4f/analysis) "”)"))
+    (path/p4f/analysis path)])
 
   (define analyses
     (remove-duplicates
@@ -79,24 +70,13 @@
       (raise-user-error
        (~a "Cannot find path to DDPA analysis at “" (path/ddpa/analysis) "”."))))
 
-  (when (member 'little-store analyses)
-    (unless (directory-exists? (path/little-store/cases))
-      (raise-user-error
-       (~a "Cannot find path to Little Store test cases at “" (path/little-store/cases) "”.")))
-    (unless (file-exists? (path/little-store/analysis))
-      (raise-user-error
-       (~a "Cannot find path to Little Store analysis at “" (path/little-store/analysis) "”."))))
-
   (when (member 'p4f analyses)
     (unless (directory-exists? (path/p4f/cases))
       (raise-user-error
        (~a "Cannot find path to P4F test cases at “" (path/p4f/cases) "”.")))
-    (unless (file-exists? (path/p4f/sbt))
+    (unless (directory-exists? (path/p4f/analysis))
       (raise-user-error
-       (~a "Cannot find path to P4F’s instance of SBT at “" (path/p4f/sbt) "”.")))
-    (unless (directory-exists? (path/p4f/working-directory))
-      (raise-user-error
-       (~a "Cannot find path to P4F’s working directory at “" (path/p4f/working-directory) "”."))))
+       (~a "Cannot find path to P4F’s analysis at “" (path/p4f/analysis) "”."))))
 
   ;; -------------------------------------------------------------------------------------------------
 
@@ -104,80 +84,64 @@
 
   (make-directory* (path/results))
 
-  (for ([experiment experiments])
-    (match-define (Experiment name/experiment tests) experiment)
-    (for ([test tests])
-      (match-define (Test name/test cases) test)
-      (for ([a-case cases])
-        (match-define (Case source (Subject analysis k) result) a-case)
-        (define path/result
-          (~a (path/results) "/"
-              (moment->iso8601 (now/moment))
-              "--experiment=" name/experiment
-              "--analysis=" analysis
-              "--test=" name/test
-              "--k=" k
-              ".txt"))
-        (define-values (path/source command)
-          (case analysis
-            [(ddpa)
-             (define path/source (~a (path/ddpa/cases) "/" source ".odefa"))
-             (values
-              path/source
-              (~a "time -p "
-                  "timeout " (timeout) " "
-                  (path/ddpa/analysis) " "
-                  "--select-context-stack=" k "ddpa "
-                  "--analyze-variables=all --report-sizes --disable-evaluation "
-                  "--disable-inconsistency-check "
-                  "< " path/source " "
-                  ">> " path/result " "
-                  "2>&1"))]
-            [(little-store)
-             (define path/source (~a (path/little-store/cases) "/" source ".odefa"))
-             (values
-              path/source
-              (~a "time -p "
-                  "timeout " (timeout) " "
-                  (path/little-store/analysis) " "
-                  "--stack-delta-size=" k " "
-                  "--analyze-variables=all --report-sizes --disable-evaluation "
-                  "--disable-inconsistency-check "
-                  "< " path/source " "
-                  ">> " path/result " "
-                  "2>&1"))]
-            [(p4f)
-             (define path/source (~a (path/p4f/cases) "/" source ".scm"))
-             (define path/statistics/directory (~a "statistics/" source))
-             (define path/statistics/file (~a path/statistics/directory "/stat-" k "-pdcfa-gc.txt"))
-             (values
-              path/source
-              (~a "cd " (path/p4f/working-directory) " && "
-                  "rm -rf '" path/statistics/directory "' && "
-                  "time -p "
-                  (path/p4f/sbt) " 'runMain org.ucombinator.cfa.RunCFA "
-                  "--k " k " "
-                  "--kalloc p4f --gc --dump-statistics --pdcfa "
-                  path/source "' "
-                  ">> " path/result " "
-                  "2>&1 && "
-                  "cat " path/statistics/file " "
-                  ">> " path/result " "
-                  "2>&1"))]
+  (for ([repetition (in-range (repetitions))])
+    (displayln (~a "Repetition " repetition " of " (repetitions)))
+    (for ([experiment experiments])
+      (match-define (Experiment name/experiment tests) experiment)
+      (for ([test tests])
+        (match-define (Test name/test cases) test)
+        (for ([a-case cases])
+          (match-define (Case source (Subject analysis k) result) a-case)
+          (define path/result
+            (~a (path/results) "/"
+                (moment->iso8601 (now/moment))
+                "--experiment=" name/experiment
+                "--analysis=" analysis
+                "--test=" name/test
+                "--k=" k
+                ".txt"))
+          (define-values (path/source command)
+            (match analysis
+              ['ddpa
+               (define path/source (~a (path/ddpa/cases) "/" source ".odefa"))
+               (values
+                path/source
+                (~a "/usr/bin/time -v "
+                    "timeout " (timeout) " "
+                    (path/ddpa/analysis) " "
+                    "--select-context-stack=" k "ddpa "
+                    "--analyze-variables=all --report-sizes --disable-evaluation "
+                    "--disable-inconsistency-check "
+                    "< " path/source " "
+                    ">> " path/result " "
+                    "2>&1"))]
+              ['p4f
+               (define path/source (~a (path/p4f/cases) "/" source ".scm"))
+               (define path/statistics/directory (~a "statistics/" source))
+               (define path/statistics/file (~a path/statistics/directory "/stat-" k "-pdcfa-gc.txt"))
+               (values
+                path/source
+                (~a "cd " (path/p4f/analysis) " && "
+                    "rm -rf '" path/statistics/directory "' && "
+                    "/usr/bin/time -v "
+                    "timeout " (timeout) " "
+                    "sbt 'runMain org.ucombinator.cfa.RunCFA "
+                    "--k " k " "
+                    "--kalloc p4f --gc --dump-statistics --pdcfa "
+                    path/source "' "
+                    ">> " path/result " "
+                    "2>&1 && "
+                    "cat " path/statistics/file " "
+                    ">> " path/result " "
+                    "2>&1"))]))
+          (cond
+            [(file-exists? path/source)
+             (display (~a command "..."))
+             (flush-output)
+             (with-output-to-file path/result (λ () (system "uptime") (displayln (~a "$ " command))))
+             (system command)
+             (displayln " done")
+             (flush-output)]
             [else
-             (raise-user-error
-              (~a "Unknown analysis;\n"
-                  " Case: " a-case "\n"
-                  " Test: " test "\n"
-                  " Experiment: " experiment))]))
-        (cond
-          [(file-exists? path/source)
-           (display (~a command "..."))
-           (flush-output)
-           (with-output-to-file path/result (λ () (displayln (~a "$ " command))))
-           (system command)
-           (displayln " done")
-           (flush-output)]
-          [else
-           (displayln
-            (~a "Cannot find test case at “" path/source "”.") (current-error-port))])))))
+             (displayln
+              (~a "Cannot find test case at “" path/source "”.") (current-error-port))]))))))
