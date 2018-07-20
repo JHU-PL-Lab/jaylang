452d4870192d34f9d9ba0169a74c56f2dcf1496d
Leandro Facchinetti <me@leafac.com>
2018-07-20 18:02:14 -0400

    Runner in bash

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,23 +1,53 @@
 #!/usr/bin/env bash
 
-# ack
-# blur
-# church
-# cpstak
-# deriv
-# eta
-# facehugger
-# flatten
-# kcfa
-# kcfa
-# loop2
-# map
-# mj09
-# primtest
-# regex
-# rsa
-# sat
-# sat
-# sat
-# state
-# tak
+declare -A TESTS=(
+  [ack]=1
+  [blur]=1
+  [church]=1
+  [cpstak]=1
+  [deriv]=1
+  [eta]=1
+  [facehugger]=1
+  [flatten]=1
+  [kcfa-2]=5
+  [kcfa-3]=7
+  [loop2]=1
+  [map]=1
+  [mj09]=1
+  [primtest]=1
+  [regex]=1
+  [rsa]=1
+  [sat-1]=4
+  [sat-2]=4
+  [sat-3]=4
+  [state]=1
+  [tak]=1
+)
+TRIALS=5
+TIMEOUT=30m
+HERE=(cd $(dirname $0) && pwd)
+DDPA=$HERE/../toploop_main.native
+SCHEME_TO_ODEFA=$HERE/../scheme-front-end/scheme-to-odefa.rkt
+P4F=$HERE/../../p4f
+
+mkdir $HERE/results
+
+for TRIAL in $(seq 1 $TRIALS)
+do
+  echo "Trial $TRIAL of $TRIALS"
+  for TEST in "${!TESTS[@]}"
+  do
+    SOURCE=$HERE/cases/$TEST.scm
+
+    EXPERIMENT=baseline
+    K=0
+
+    ANALYSIS=ddpa
+    RESULT=$HERE/results/$(date --iso-8601=seconds)--experiment=$EXPERIMENT--analysis=$ANALYSIS--test=$TEST--k=$K.txt
+    cat $SOURCE | racket $SCHEME_TO_ODEFA | /usr/bin/timeout $TIMEOUT /usr/bin/time -v $DDPA --select-context-stack=${K}ddpa --analyze-variables=all --report-sizes --disable-evaluation --disable-inconsistency-check &>> $RESULT
+
+    ANALYSIS=p4f
+    RESULT=$HERE/results/$(date --iso-8601=seconds)--experiment=$EXPERIMENT--analysis=$ANALYSIS--test=$TEST--k=$K.txt
+    (cd $P4F && rm -rf statistics/ && /usr/bin/timeout $TIMEOUT /usr/bin/time -v sbt "runMain org.ucombinator.cfa.RunCFA --k $K --kalloc p4f --gc --dump-statistics --pdcfa $SOURCE" &>> $RESULT && cat statistics/$TEST/stat-$K-pdcfa-gc.txt &>> RESULT)
+  done
+done
