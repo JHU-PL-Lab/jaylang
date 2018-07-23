4bde84ce03aef98654bcd51b9746158a8801e8c4
Leandro Facchinetti <me@leafac.com>
2018-07-23 15:56:23 -0400

    Prepare runner

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -x
 
-declare -A TESTS=(
+declare -A CASES=(
   [ack]=1
   [blur]=1
   [church]=1
@@ -27,12 +27,17 @@ TRIALS=5
 TIMEOUT=30m
 
 HERE=$(cd $(dirname $0) && pwd)
+CASES_PATH=$HERE/cases
+RESULTS_PATH=$HERE/results
 DDPA=$HERE/..
 DDPA_TOPLOOP=$DDPA/toploop_main.native
-SCHEME_TO_ODEFA=$DDPA/../scheme-front-end/scheme-to-odefa.rkt
-P4F=$DDPA/../../p4f
+SCHEME_TO_ODEFA=$DDPA/scheme-front-end/scheme-to-odefa.rkt
+P4F=$DDPA/../p4f
 P4F_CLASSPATH=$P4F/target/scala-2.11/classes
 P4F_STATISTICS=$P4F/statistics
+function result {
+  RESULT=$RESULTS_PATH/experiment=$EXPERIMENT--case=$CASE--analysis=$ANALYSIS--k=$K--$(date --iso-8601=seconds).txt
+}
 
 (cd $DDPA && git rev-parse HEAD)
 ocaml -version
@@ -42,12 +47,6 @@ racket --version
 java -version
 sbt sbtVersion
 scala -version
-
-mkdir $HERE/results
-
-function result {
-  RESULT=$HERE/results/$(date --iso-8601=seconds)--experiment=$EXPERIMENT--analysis=$ANALYSIS--test=$TEST--k=$K.txt
-}
 
 function ddpa {
   ANALYSIS=ddpa
@@ -59,26 +58,28 @@ function p4f {
   ANALYSIS=p4f
   result
   rm -rf $P4F_STATISTICS
-  /usr/bin/time -v /usr/bin/timeout --foreground $TIMEOUT scala -cp $P4F_CLASSPATH org.ucombinator.cfa.RunCFA --k $K --kalloc p4f --gc --dump-statistics --pdcfa $SOURCE &>> $RESULT
-  if $?
+  (cd $PF4 && /usr/bin/time -v /usr/bin/timeout --foreground $TIMEOUT scala -cp $P4F_CLASSPATH org.ucombinator.cfa.RunCFA --k $K --kalloc p4f --gc --dump-statistics --pdcfa $SOURCE &>> $RESULT)
+  if [[ $? ]]
   then
-    cat $P4F_STATISTICS/$TEST/stat-$K-pdcfa-gc.txt &>> RESULT
+    cat $P4F_STATISTICS/$CASE/stat-$K-pdcfa-gc.txt &>> $RESULT
   else
     pkill java
   fi
 }
 
+mkdir $RESULTS_PATH
+
 for TRIAL in $(seq 1 $TRIALS)
 do
-  for TEST in "${!TESTS[@]}"
+  for CASE in "${!CASES[@]}"
   do
-    SOURCE=$HERE/cases/$TEST.scm
+    SOURCE=$CASES_PATH/$CASE.scm
     EXPERIMENT=baseline
     K=0
     ddpa
     p4f
     EXPERIMENT=polyvariance
-    K=${TESTS[$TEST]}
+    K=${CASES[$CASE]}
     ddpa
     K=1
     p4f
