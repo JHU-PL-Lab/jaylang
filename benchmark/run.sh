65ae37bda22268f1d5c5e8f29cf6d42fbae667fa
Leandro Facchinetti <me@leafac.com>
2018-07-24 09:39:20 -0400

    Fix syntax

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -70,7 +70,7 @@ function p4f {
   ANALYSIS=p4f
   result
   rm -rf "${P4F_STATISTICS}"
-  if [[ (cd "${P4F}" && /usr/bin/time -v /usr/bin/timeout --foreground "${TIMEOUT}" scala -cp "${P4F_CLASSPATH}" org.ucombinator.cfa.RunCFA --k "${K}" --kalloc p4f --gc --dump-statistics --pdcfa "${SOURCE}" &>> "${RESULT}") ]]
+  if (cd "${P4F}" && /usr/bin/time -v /usr/bin/timeout --foreground "${TIMEOUT}" scala -cp "${P4F_CLASSPATH}" org.ucombinator.cfa.RunCFA --k "${K}" --kalloc p4f --gc --dump-statistics --pdcfa "${SOURCE}" &>> "${RESULT}")
   then
     cat "${P4F_STATISTICS}/${CASE}/stat-${K}-pdcfa-gc.txt" &>> "${RESULT}"
   else
