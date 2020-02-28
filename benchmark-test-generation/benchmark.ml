e5cc48145b6b23308d0dc508896ae060fc263f2a
Weng Shiwei <arbipher@gmail.com>
2020-02-28 17:58:32 -0500

    update benchmark script

--- benchmark-test-generation/benchmark.ml
+++ benchmark-test-generation/benchmark.ml
@@ -29,9 +29,14 @@ let benchmark n : unit t =
   let test_path = "test-sources/" ^ test
   and test_result = working_path ^ (test ^ "_" ^ (string_of_int n)) ^ ".txt" in
   print test
-  >> stdout_to test_result (run "./test_generator" ["-t"; "target"; test_path])
+  (* time gtimeout --foreground 1m ls *)
+  >> outputs_to ~append:() test_result (call ["time"; "gtimeout"; "--foreground"; "1m" ;"./test_generator"; "-t"; "target"; test_path])
   >> echo @@ "done - " ^ (string_of_int n)
 (* |- run "tee" ["-a"; result_file] *)
+
+let collect_result : unit t =
+  (run "date" ["-u"; "+%Y-%m-%d-%H:%M:%SZ"])
+  |- (run "xargs" ["-I"; "%"; "mv"; working_path;"result/evaluate-%"])
 
 let countdown n task : unit t =
   if n > 0 then
@@ -42,6 +47,7 @@ let countdown n task : unit t =
 let main  =
   ignore @@ 
   eval (
-    prepare >>
-    (countdown repeat_n benchmark))
+    prepare
+    >> (countdown repeat_n benchmark)
+    >> collect_result)
 
