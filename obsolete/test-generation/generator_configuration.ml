8253242b6bdbdab4e70cb3c03db7376331177eee
Weng Shiwei <arbipher@gmail.com>
2021-10-30 19:23:23 -0400

    Remove obsolete files.

--- obsolete/test-generation/generator_configuration.ml
+++
@@ -1,9 +0,0 @@
-(**
-   This module defines the form of a configuration for test generation.
-*)
-
-open Odefa_ddpa;;
-
-type configuration = {
-  conf_context_model : (module Ddpa_context_stack.Context_stack);
-};;
--- obsolete/test-generation/generator_constants.ml
+++
@@ -1,3 +0,0 @@
-(** Contains general constants used throughout the application. *)
-
-let version : string = "0.1";;
