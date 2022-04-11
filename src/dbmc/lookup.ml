7a10b026071875e67a70c45d681634126bbf4a8c
Weng Shiwei <arbipher@gmail.com>
2022-04-10 21:07:31 -0400

    Some cleanup.

--- src/dbmc/lookup.ml
+++ src/dbmc/lookup.ml
@@ -12,22 +12,13 @@ module U = Global_state.Unroll
 
 let[@landmark] run ~(config : Global_config.t) ~(state : Global_state.t)
     job_queue : unit =
-  let target = state.target in
-  let map = state.block_map in
-  let x_first = state.first in
   (* let block0 = Tracelet.cut_before true target block in *)
-  let block0 = Tracelet.find_by_id target map in
-
-  let add_phi = Global_state.add_phi state in
-
-  let find_or_add_node key block node_parent =
-    Global_state.find_or_add_node state key block node_parent |> snd
-  in
+  let block0 = Tracelet.find_by_id state.target state.block_map in
 
   (* reset and init *)
   Solver.reset () ;
   Riddler.reset () ;
-  state.phis_z3 <- [ Riddler.pick_at_key (Lookup_key.start target) ] ;
+  state.phis_z3 <- [ Riddler.pick_at_key (Lookup_key.start state.target) ] ;
 
   (* async handler *)
   (Lwt.async_exception_hook :=
@@ -44,9 +35,14 @@ let[@landmark] run ~(config : Global_config.t) ~(state : Global_state.t)
   let module RS = (val (module struct
                          let state = state
                          let config = config
-                         let add_phi = add_phi
-                         let x_first = x_first
-                         let find_or_add_node = find_or_add_node
+                         let add_phi = Global_state.add_phi state
+                         let x_first = state.first
+
+                         let find_or_add_node key block node_parent =
+                           Global_state.find_or_add_node state key block
+                             node_parent
+                           |> snd
+
                          let block_map = state.block_map
                        end) : Ruler.S)
   in
@@ -54,7 +50,7 @@ let[@landmark] run ~(config : Global_config.t) ~(state : Global_state.t)
   (* block works similar to env in a common interpreter *)
   let[@landmark] rec lookup (this_key : Lookup_key.t) block () : unit Lwt.t =
     let x, xs, _r_stk = Lookup_key.to_parts this_key in
-    let gate_tree = Global_state.find_node_exn state this_key block in
+    let this_node = Global_state.find_node_exn state this_key block in
 
     let run_task key block = run_eval key block lookup in
 
@@ -71,25 +67,22 @@ let[@landmark] run ~(config : Global_config.t) ~(state : Global_state.t)
     let apply_rule () =
       let open Rule in
       match rule with
-      | Discovery_main p -> R.discovery_main p this_key gate_tree
+      | Discovery_main p -> R.discovery_main p this_key this_node
       | Discovery_nonmain p ->
-          R.discovery_nonmain p this_key gate_tree block run_task
-      | Input p -> R.input p this_key gate_tree block run_task
-      | Discard p -> R.discard p this_key gate_tree block run_task
-      | Alias p -> R.alias p this_key gate_tree block run_task
-      | Binop b -> R.binop b this_key gate_tree block run_task
-      | Record_start p -> R.record_start p this_key gate_tree block run_task
-      | Record_end p -> R.record_end p this_key gate_tree block run_task
-      | Cond_top cb -> R.cond_top cb this_key gate_tree block run_task
-      | Cond_btm p -> R.cond_btm p this_key gate_tree block run_task
-      | Fun_enter_local p -> R.fun_enter_local p this_key gate_tree run_task
+          R.discovery_nonmain p this_key this_node block run_task
+      | Input p -> R.input p this_key this_node block run_task
+      | Discard p -> R.discard p this_key this_node block run_task
+      | Alias p -> R.alias p this_key this_node block run_task
+      | Binop b -> R.binop b this_key this_node block run_task
+      | Record_start p -> R.record_start p this_key this_node block run_task
+      | Record_end p -> R.record_end p this_key this_node block run_task
+      | Cond_top cb -> R.cond_top cb this_key this_node block run_task
+      | Cond_btm p -> R.cond_btm p this_key this_node block run_task
+      | Fun_enter_local p -> R.fun_enter_local p this_key this_node run_task
       | Fun_enter_nonlocal p ->
-          R.fun_enter_nonlocal p this_key gate_tree run_task
-      | Fun_exit p -> R.fun_exit p this_key gate_tree block run_task
-      | Mismatch ->
-          Node.update_rule gate_tree Node.mismatch ;
-          add_phi this_key (Riddler.mismatch this_key) ;
-          Lookup_result.fail_lwt x
+          R.fun_enter_nonlocal p this_key this_node run_task
+      | Fun_exit p -> R.fun_exit p this_key this_node block run_task
+      | Mismatch -> R.mismatch this_key this_node
     in
     let%lwt _rule_result = apply_rule () in
     let%lwt _ =
@@ -108,6 +101,6 @@ let[@landmark] run ~(config : Global_config.t) ~(state : Global_state.t)
     Lwt.return_unit
   in
 
-  let key_target = Lookup_key.of_parts target [] Rstack.empty in
+  let key_target = Lookup_key.of_parts state.target [] Rstack.empty in
   let _ = Global_state.init_node state key_target state.root_node in
   run_eval key_target block0 lookup
