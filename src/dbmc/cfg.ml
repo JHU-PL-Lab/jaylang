4e1bce772f02e0a1ca57ab06ef4a02928b153a52
Weng Shiwei <arbipher@gmail.com>
2022-07-15 18:43:15 -0400

    Trivia.

--- src/dbmc/cfg.ml
+++ src/dbmc/cfg.ml
@@ -297,10 +297,12 @@ let annotate e pt : block Ident_map.t =
   and cfg = cfg_of e
   (* and id_first = first_var e *)
   and ret_to_fun_def_map = make_ret_to_fun_def_mapping e
-  and para_to_fun_def_map = make_para_to_fun_def_mapping e
-  and acl =
-    Unannotated_clause (lift_clause @@ Ident_map.find pt (clause_mapping e))
-  in
+  and para_to_fun_def_map = make_para_to_fun_def_mapping e in
+  let pt_clause = Ident_map.find pt (clause_mapping e) in
+  (* let is_abort_clause =
+       match pt_clause with Clause (_, Abort_body) -> true | _ -> false
+     in *)
+  let acl = Unannotated_clause (lift_clause pt_clause) in
 
   (* let debug_bomb = ref 20 in *)
   let visited = ref Annotated_clause_set.empty in
