b0ca5dec96e7af18737d9e377d1feef3eef34de8
Weng Shiwei <arbipher@gmail.com>
2022-07-22 17:03:26 -0400

    WIP: debug testing timeout.

--- test/dbmc/test_dbmc.ml
+++ test/dbmc/test_dbmc.ml
@@ -2,13 +2,31 @@ open Core
 open Dbmc
 
 type config = {
+  timeout : Time.Span.t option;
   engine : Global_config.engine;
+  test_path : string;
   is_instrumented : bool;
   initial_stride : int;
 }
 
 module Cmd_parser = struct
   open Cmdliner
+
+  let timeout_conv =
+    let parser s = Result.Ok (Scanf.sscanf s "%d" Time.Span.of_int_sec) in
+    let printer oc s = Fmt.string oc @@ Time.Span.to_string_hum s in
+    Arg.(conv (parser, printer))
+
+  let timeout =
+    let doc = "Timeout in seconds per test" in
+    Arg.(
+      value
+      & opt (some timeout_conv) (Some (Time.Span.of_int_sec 5))
+      & info [ "tm"; "timeout" ] ~docv:"TIMEOUT" ~doc)
+
+  let no_timeout =
+    let doc = "No timeout" in
+    Arg.(value & flag & info [ "tmu"; "no-timeout" ] ~docv:"NOTIMEOUT" ~doc)
 
   let engine_conv =
     let parser s =
@@ -31,6 +49,12 @@ module Cmd_parser = struct
       & opt engine_conv Global_config.E_dbmc
       & info [ "te"; "engine" ] ~docv:"ENGINE" ~doc)
 
+  let test_path =
+    let doc = "Path for test cases" in
+    Arg.(
+      value & opt string "test-sources"
+      & info [ "tp"; "test-path" ] ~docv:"TESTPATH" ~doc)
+
   let is_instrumented =
     let doc = "Instrument clauses." in
     Arg.(
@@ -45,11 +69,15 @@ module Cmd_parser = struct
       & opt int Global_config.default_config.stride_init
       & info [ "ts"; "stride" ] ~docv:"STRIDE" ~doc)
 
-  let make_config engine is_instrumented initial_stride =
-    { engine; is_instrumented; initial_stride }
+  let make_config timeout no_timeout engine test_path is_instrumented
+      initial_stride =
+    let timeout = if no_timeout then None else timeout in
+    { timeout; engine; test_path; is_instrumented; initial_stride }
 
   let config =
-    Term.(const make_config $ engine $ is_instrumented $ initial_stride)
+    Term.(
+      const make_config $ timeout $ no_timeout $ engine $ test_path
+      $ is_instrumented $ initial_stride)
 end
 
 open Cmd_parser
@@ -107,9 +135,13 @@ let test_one_file test_config testname () =
           match List.hd expectation.inputs with
           | Some inputs ->
               let checked = Main.check_input ~config src inputs in
+              ignore @@ Lwt.return (Alcotest.fail "immediate1") ;
+
               Alcotest.(check bool) "equal" true checked
           | None -> (
               let inputss = Main.search_input ~config src in
+              ignore @@ failwith "immediate2" ;
+              ignore @@ Alcotest.fail "immediate2-1" ;
               match List.hd inputss with
               | Some inputs ->
                   let expected_inputs = List.hd_exn expectation.inputs in
@@ -118,6 +150,7 @@ let test_one_file test_config testname () =
                   Alcotest.(check (list int_option_checker))
                     "equal" inputs expected_inputs
               | None ->
+                  ignore @@ failwith "immediate3" ;
                   Alcotest.(check int)
                     "equal" 0
                     (List.length expectation.inputs)))
@@ -139,10 +172,13 @@ let test_one_file test_config testname () =
      raise exc *)
 
 let test_one_file_lwt testname _switch test_config =
-  try%lwt
-    Lwt_unix.with_timeout 5.0 (fun () ->
-        Lwt.return (test_one_file test_config testname ()))
-  with Lwt_unix.Timeout -> Lwt.return (Alcotest.fail "timeout")
+  match test_config.timeout with
+  | Some t -> (
+      try%lwt
+        Lwt_unix.with_timeout (Time.Span.to_sec t) (fun () ->
+            Lwt.return (test_one_file test_config testname ()))
+      with Lwt_unix.Timeout -> failwith "timeoutout")
+  | None -> Lwt.return (test_one_file test_config testname ())
 
 (* let test_one_file_lwt testname _switch () =
    Lwt.return (test_one_file testname ()) *)
@@ -150,9 +186,8 @@ let test_one_file_lwt testname _switch test_config =
 (* let test_one_file_timed testname () = test_one_file testname () *)
 (* try timeout (test_one_file testname) () 5 with Timeout -> raise Timeout *)
 
-let () =
-  let path = "test-sources" in
-  let grouped_testfiles = group_all_files path in
+let main top_config =
+  let grouped_testfiles = group_all_files top_config.test_path in
   let grouped_tests =
     List.map grouped_testfiles ~f:(fun (group_name, test_names) ->
         ( group_name,
@@ -169,3 +204,14 @@ let () =
   Alcotest_lwt.run_with_args "DBMC" config grouped_tests ;
   Dbmc.Log.close () ;
   ()
+
+let top_config = ref None
+
+let () =
+  (ignore
+  @@ Cmdliner.Cmd.(
+       eval
+       @@ v (info "test")
+            Cmdliner.Term.(
+              const (fun config -> top_config := Some config) $ config))) ;
+  main (Option.value_exn !top_config)
