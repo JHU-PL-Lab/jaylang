b4c0529129f4f3b32eb8fe2a49e0139746765e3b
earl-wu <earlwu96@gmail.com>
2022-09-02 21:08:32 -0400

    Fixed bug in parsable flag for the translator

--- src/translator-main/translator.ml
+++ src/translator-main/translator.ml
@@ -32,10 +32,18 @@ let main () : unit =
         (* Typed -> Untyped *)
         Bluejay.Bluejay_to_jay.transform_bluejay bluejay_ast_internal.body
     in
+    let translation_ctx = 
+      if options.ta_parseable
+      then
+          Jay.Jay_to_jayil_monad.new_translation_context ~is_natodefa:true
+          ~suffix:"___" ()
+      else
+          Jay.Jay_to_jayil_monad.new_translation_context ()
+    in
     let jay_ast = Bluejay.Bluejay_ast_internal.to_jay_expr_desc core_ast in
     let is_instrumented = options.ta_instrument in
     let post_inst_ast, _odefa_inst_maps, _on_odefa_maps =
-      Jay_to_jayil.translate ~is_instrumented:is_instrumented jay_ast
+      Jay_to_jayil.translate ~is_instrumented:is_instrumented ~translation_context:(Some translation_ctx) jay_ast
     in
     let result_expr =
       if options.ta_parseable
@@ -58,20 +66,28 @@ let main () : unit =
     let expr_string = Jay.Jay_ast_pp.show_expr_desc jay_ast in
     print_endline expr_string
   | Jay_to_jayil ->
-      let jay_ast = 
-        Jay_ast.new_expr_desc @@ Jay_parse.parse_program IO.stdin 
-      in
-      let is_instrumented = options.ta_instrument in
-      let post_inst_ast, _odefa_inst_maps, _on_odefa_maps =
-        Jay_to_jayil.translate ~is_instrumented:is_instrumented jay_ast
-      in
-      let result_expr =
-        if options.ta_parseable
-        then map_expr_vars purge_special_symbols_jayil post_inst_ast
-        else post_inst_ast
-      in
-      let expr_string = show_expr result_expr in
-      print_endline expr_string
+    let translation_ctx = 
+      if options.ta_parseable
+      then
+          Jay.Jay_to_jayil_monad.new_translation_context ~is_natodefa:true
+          ~suffix:"___" ()
+      else
+          Jay.Jay_to_jayil_monad.new_translation_context ()
+    in
+    let jay_ast = 
+      Jay_ast.new_expr_desc @@ Jay_parse.parse_program IO.stdin 
+    in
+    let is_instrumented = options.ta_instrument in
+    let post_inst_ast, _odefa_inst_maps, _on_odefa_maps =
+      Jay_to_jayil.translate ~is_instrumented:is_instrumented ~translation_context:(Some translation_ctx) jay_ast
+    in
+    let result_expr =
+      if options.ta_parseable
+      then map_expr_vars purge_special_symbols_jayil post_inst_ast
+      else post_inst_ast
+    in
+    let expr_string = show_expr result_expr in
+    print_endline expr_string
   | Scheme_to_jay ->
       raise @@ Jhupllib.Utils.Not_yet_implemented "scheme-to-jay"
 ;;
