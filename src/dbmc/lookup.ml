a29b948069427c669534ad70847506b858aef711
Weng Shiwei <arbipher@gmail.com>
2022-10-06 16:12:27 -0400

    Fix merging bug.

--- src/dbmc/lookup.ml
+++ src/dbmc/lookup.ml
@@ -131,7 +131,7 @@ let[@landmark] run_dbmc ~(config : Global_config.t) ~(state : Global_state.t) :
     let rule = Rule.rule_of_runtime_status key state.block_map in
     let term_detail = Term_detail.mk_detail ~rule ~key in
 
-    Out_channel.flush (Option.value_exn !Log.saved_oc) ;
+    Option.iter !Log.saved_oc ~f:Out_channel.flush ;
 
     Hashtbl.add_exn state.term_detail_map ~key ~data:term_detail ;
 
