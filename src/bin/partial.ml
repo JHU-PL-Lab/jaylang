e8d39258a3216af1db9efad208f890bae662779c
Ricky Cheng <rcheng15@jhu.edu>
2023-02-09 18:33:41 -0500

    Fix types in bin/partial

--- src/bin/partial.ml
+++ src/bin/partial.ml
@@ -5,11 +5,11 @@ let source_file = ref ""
 let anon_fun _ = ()
 
 let run_program source =
-  ignore @@ Partial_evaluator.eval source ;
+  ignore @@ Partial_evaluator.eval_file source ;
   ()
 
 let () =
   Arg.parse
-    [ ("-i", Arg.Set_string source_file, "Iutput source file") ]
+    [ ("-i", Arg.Set_string source_file, "Input source file") ]
     anon_fun usage_msg ;
   run_program !source_file
