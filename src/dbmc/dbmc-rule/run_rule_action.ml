3fe5efd68790a3bdf839c1f0f8506b473bad03b2
Weng Shiwei <arbipher@gmail.com>
2023-02-24 01:32:44 -0500

    WIP (update complete status).

--- src/dbmc/dbmc-rule/run_rule_action.ml
+++ src/dbmc/dbmc-rule/run_rule_action.ml
@@ -25,6 +25,16 @@ let add_phi_edge state term_detail edge =
   List.iter (Rule_action.phis_of edge)
     ~f:(Global_state.add_phi state term_detail)
 
+let exists_status map lookups f =
+  List.exists lookups ~f:(fun lookup ->
+      let (detail : Term_detail.t) = Hashtbl.find_exn map lookup in
+      f detail.status)
+
+let for_all_status map lookups f =
+  List.for_all lookups ~f:(fun lookup ->
+      let (detail : Term_detail.t) = Hashtbl.find_exn map lookup in
+      f detail.status)
+
 let rec run run_task unroll (state : Global_state.t)
     (term_detail : Term_detail.t) rule_action =
   let loop rule_action = run run_task unroll state term_detail @@ rule_action in
@@ -66,75 +76,95 @@ let rec run run_task unroll (state : Global_state.t)
   | Chain e ->
       (* Fmt.pr "\n[Chain start]%a = %a -> " Lookup_key.pp e.sub Lookup_key.pp
          e.pub ; *)
+      (* Fmt.pr "\n[pre_push][X]%a = %a in %a " Lookup_key.pp e.sub Lookup_key.pp
+           e.pub Lookup_status.pp term_detail.status ;
+         Fmt.pr "\n[pre_push][R]<- %a in %a" Lookup_key.pp r.from
+           Lookup_status.pp r.status ; *)
       let part1_done = ref false in
-      let cb key (r : Lookup_result.t) =
-        (match r.status with
-        | Fail -> ()
-        | Complete | Good -> (
-            part1_done := true ;
-            let edge = e.next key r in
-            match edge with
+      let part1_cb key (r : Lookup_result.t) =
+        if Lookup_status.is_complete_or_fail r.status
+        then part1_done := true
+        else () ;
+
+        Lookup_status.iter_ok r.status (fun () ->
+            match e.next key r with
             | Some edge ->
                 term_detail.sub_lookups <-
                   term_detail.sub_lookups @ [ Rule_action.sub_of edge ] ;
                 loop edge
-            | None -> ())) ;
+            | None -> ()) ;
         Lwt.return_unit
       in
+      let part2_done = ref false in
       let pre_push (r : Lookup_result.t) =
-        (* Fmt.pr "\n[pre_push][X]%a = %a in %a " Lookup_key.pp e.sub Lookup_key.pp
-             e.pub Lookup_status.pp term_detail.status ;
-           Fmt.pr "\n[pre_push][R]<- %a in %a" Lookup_key.pp r.from
-             Lookup_status.pp r.status ; *)
-        (match r.status with
-        | Fail -> ()
-        | Complete | Good -> if !part1_done then () else ()) ;
+        Lookup_status.iter_ok r.status (fun () ->
+            if !part1_done && not !part2_done
+            then
+              if for_all_status state.term_detail_map term_detail.sub_lookups
+                   Lookup_status.is_complete_or_fail
+              then (
+                part2_done := true ;
+                let this_status =
+                  exists_status state.term_detail_map term_detail.sub_lookups
+                    Lookup_status.is_complete
+                  |> Lookup_status.complete_or_fail
+                in
+                term_detail.status <- this_status)
+              else ()) ;
         Some r
-        (* ; *)
       in
-
-      (* =
-           (match r.status with
-           | Fail | Complete ->
-               Lwt.async (fun () ->
-                   (* Fmt.pr "\n[Chain end (step1)]%a = %a -> " Lookup_key.pp e.sub
-                      Lookup_key.pp e.pub ; *)
-                   let%lwt all_complete =
-                     Lwt_list.for_all_s
-                       (fun sub_key ->
-                         (* Fmt.pr "%a; " Lookup_key.pp sub_key ; *)
-                         let sub_s = U.get_stream unroll sub_key in
-                         Lwt_stream.junk_while Lookup_result.is_good sub_s ;%lwt
-                         Lwt.map Lookup_result.is_complete (Lwt_stream.next sub_s))
-                       term_detail.sub_lookups
-                   in
-                   (* Fmt.pr "\n[Chain end (step2)]%a = %a <- " Lookup_key.pp e.sub
-                      Lookup_key.pp e.pub ; *)
-                   term_detail.is_complete_or_fail <- all_complete ;
-                   Lwt.return_unit)
-           | Good -> ()) ;
-         in *)
-      U.by_bind_u unroll e.sub e.pub cb ;
+      U.by_bind_u unroll e.sub e.pub part1_cb ;
       U.set_pre_push unroll e.sub pre_push ;
       run_task e.pub
   | Sequence e ->
       init_list_counter state term_detail e.sub ;
+      let part1_done = ref false in
+      let part1_cb _key (r : Lookup_result.t) =
+        if Lookup_status.is_complete_or_fail r.status
+        then part1_done := true
+        else () ;
 
-      let cb _key (r : Lookup_result.t) =
-        let i = fetch_list_counter state term_detail e.sub in
-        let next = e.next i r in
-        (match next with
-        | Some (phi_i, next) ->
-            let phi = Riddler.list_append e.sub i phi_i in
-            add_phi term_detail phi ;
-            loop next
-        | None ->
-            add_phi term_detail (Riddler.list_append e.sub i Riddler.false_)) ;
+        Lookup_status.iter_ok r.status (fun () ->
+            let i = fetch_list_counter state term_detail e.sub in
+            let next = e.next i r in
+            match next with
+            | Some (phi_i, next) ->
+                let phi = Riddler.list_append e.sub i phi_i in
+                add_phi term_detail phi ;
+                term_detail.sub_lookups <-
+                  term_detail.sub_lookups @ [ Rule_action.sub_of next ] ;
+                loop next
+            | None ->
+                add_phi term_detail (Riddler.list_append e.sub i Riddler.false_)) ;
         Lwt.return_unit
       in
-      U.by_bind_u unroll e.sub e.pub cb ;
+      let part2_done = ref false in
+      let pre_push (r : Lookup_result.t) =
+        Lookup_status.iter_ok r.status (fun () ->
+            if !part1_done && not !part2_done
+            then
+              if for_all_status state.term_detail_map term_detail.sub_lookups
+                   Lookup_status.is_complete_or_fail
+              then (
+                part2_done := true ;
+                let this_status =
+                  exists_status state.term_detail_map term_detail.sub_lookups
+                    Lookup_status.is_complete
+                  |> Lookup_status.complete_or_fail
+                in
+                term_detail.status <- this_status)
+              else ()) ;
+        Some r
+      in
+      U.by_bind_u unroll e.sub e.pub part1_cb ;
+      U.set_pre_push unroll e.sub pre_push ;
       run_task e.pub
   | Or_list e ->
       if e.unbound then init_list_counter state term_detail e.sub else () ;
-      List.iter e.elements ~f:(fun e -> loop e)) ;
+      (* TODO *)
+      List.iter e.elements ~f:(fun e ->
+          term_detail.sub_lookups <-
+            term_detail.sub_lookups @ [ Rule_action.sub_of e ] ;
+          loop e)) ;
+
   add_phi_edge state term_detail rule_action
--- src/dbmc/structure/lookup_status.ml
+++ src/dbmc/structure/lookup_status.ml
@@ -16,8 +16,20 @@ type t = Good | Fail | Complete
 let show = function Good -> "good" | Fail -> "fail" | Complete -> "done"
 let pp = Fmt.(using show string)
 let is_ok = function Good | Complete -> true | _ -> false
+let is_complete = function Complete -> true | _ -> false
+let is_complete_or_fail = function Complete | Fail -> true | _ -> false
+
+let iter ~good ~complete ~fail = function
+  | Good -> good ()
+  | Complete -> complete ()
+  | Fail -> fail ()
+
+let iter_good r f = iter ~good:f ~complete:Fn.ignore ~fail:Fn.ignore r
+let iter_ok r f = iter ~good:f ~complete:f ~fail:Fn.ignore r
+
 (* let is_good = function Good -> true | _ -> false *)
 (* let is_complete x = match x.status with Complete -> true | _ -> false
 
-   let is_complete_or_fail x =
-     match x.status with Complete | Fail -> true | _ -> false *)
+   *)
+
+let complete_or_fail b = if b then Complete else Fail
