e6efa30cfced6ea40fe0ed1bfb0be156cedda95d
Weng Shiwei <arbipher@gmail.com>
2023-08-17 17:09:35 -0400

    WIP: add the annotation from the new analysis (not tested)

--- src/jil-analysis/cfg_of_analysis.ml
+++ src/jil-analysis/cfg_of_analysis.ml
@@ -54,29 +54,67 @@ let to_cfg e =
     | Cond cb -> { block with clauses = clauses_mapper block.clauses }
   in
 
-  (* let update_block block =
-       let open Dj_common.Cfg in
-       match block.kind with
-       | Main -> block
-       | Fun fb ->
-           (* ask all Appl clauses to see if they call *)
-           let callsites =
-             []
-             (* Hashtbl.fold result_map ~init:[] ~f:(fun ~key:x ~data:vs acc ->
-                if Set.exists vs ~f:(fun v -> block.id) then x::acc else
-                acc) *)
-           in
-           { block with kind = Fun { fb with callsites } }
-       | Cond cb -> { block with kind = Cond cb }
-     in *)
-
   (* update all clauses in each blocks *)
   block_map := Jayil.Ast.Ident_map.map update_clauses !block_map ;
-  (* update fun and cond block *)
+
+  (* update fun and cond block
+
+     iterate all key, result_value pairs.
+     for each key, find its clauses and case the clause
+      1. application clause `x = f _`. Iterate the possible closures results for f. If we are in the f's closure (function) body, we may come from `x`. The fid of _closure (function) body_ is retrieved from `para_to_fun_def_map`, the block associated with fid is retrieved from `block_map`, then we update the block by adding a new possible callsite `x`.
+      2. condition clause `x = c ? e1 : e2`. The design is `x` points to a `cond_both` block, and `e1` and `e2` points to a `cond_case` block. A `cond_both` blocks contains two optional `cond_case` blocks. A `cond_case` block has a `possible` field. `cfg_of_course` creats these three blocks, with optional values not-none and `possible` to be `true`. Then the analysis updates the `possible` to be false and the optional block to be `none` if unreachable.
+  *)
   Hashtbl.iteri result_map ~f:(fun ~key:x ~data:vs ->
+      let open Dj_common.Cfg in
       let open Jayil.Ast in
       let cl = Ident_map.find x id_to_clause_map in
-      match cl with Clause (_, Appl_body (Var (f, _), Var _)) -> () | _ -> ()) ;
+      match cl with
+      | Clause (Var (xc, _), Appl_body (Var (f, _), Var _)) ->
+          Hashtbl.find_exn result_map f
+          |> Set.to_list
+          |> List.iter ~f:(function
+               | AInt -> ()
+               | ABool _ -> ()
+               | Any -> ()
+               | AClosure (x, _, _) ->
+                   let fid = Jayil.Ast.Ident_map.find x para_to_fun_def_map in
+                   let block_map' =
+                     Jayil.Ast.Ident_map.update_stdlib fid
+                       (function
+                         | Some block ->
+                             let fblock = cast_to_fun_block_info block in
+                             let fblock' =
+                               {
+                                 fblock with
+                                 callsites = xc :: fblock.callsites;
+                               }
+                             in
+                             Some { block with kind = Fun fblock' }
+                         | None -> failwith "must have this fblock")
+                       !block_map
+                   in
+                   block_map := block_map')
+      | Clause (Var (xc, _), Conditional_body (Var (c, _), _, _)) ->
+          let vs = Hashtbl.find_exn result_map c in
+          let cond_both = find_cond_blocks xc !block_map in
+          let may_be_true =
+            Set.exists ~f:(function ABool true -> true | _ -> false) vs
+          in
+          let may_be_false =
+            Set.exists ~f:(function ABool false -> true | _ -> false) vs
+          in
+          let set_impossible beta =
+            let beta_block =
+              if beta
+              then Option.value_exn cond_both.else_
+              else Option.value_exn cond_both.then_
+            in
+            set_block_impossible block_map beta_block
+          in
+          if not may_be_true then set_impossible true ;
+          if not may_be_false then set_impossible false ;
+          ()
+      | _ -> ()) ;
 
   (* block_map := Jayil.Ast.Ident_map.map update_block !block_map ; *)
   !block_map
