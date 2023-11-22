54d9e6c4f66c2ed3cce2476e42d0a157571d912c
earl-wu <earlwu96@gmail.com>
2023-11-22 14:34:20 -0500

    Minor changes to test

--- test/sato/typed-natodefa-tests/dependent_type_test_1.expect.s
+++ test/sato/typed-natodefa-tests/dependent_type_test_1.expect.s
@@ -1,7 +1,7 @@
 ((found_at_clause
-  "let f (a : int) : (B a -> int) = let g (b : B a) : int = if a > 0 then 1 else if b then true else 2 in g in f")
+  "let g (b : B a) : int = if a > 0 then 1 else if b then true else 2 in g")
  (number_of_errors 1)
  (error_list
   ((Type_error
-    ((t_var f) (t_expected_type "((a : int) -> (B a -> int))")
-     (t_actual_type "((a : int) -> (B a -> bool))"))))))
+    ((t_var g) (t_expected_type "((b : B a) -> int)")
+     (t_actual_type "((b : B a) -> bool)"))))))
