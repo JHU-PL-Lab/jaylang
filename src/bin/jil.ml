2128cca0bc40dc21a40f93c8aa04611e966857d6
brandonzstride <brandonzstride@hotmail.com>
2023-12-10 15:00:36 -0500

    revert jil

--- src/bin/jil.ml
+++ src/bin/jil.ml
@@ -5,8 +5,6 @@ let usage_msg = "jil -i <file> [<input_i>]"
 let source_file = ref ""
 let inputs = ref []
 
-let mode = ref ""
-
 let anon_fun i_raw =
   let this_i = int_of_string_opt i_raw in
   inputs := !inputs @ [ this_i ]
@@ -16,30 +14,18 @@ let run_program source =
   let session =
     {
       (Interpreter.make_default_session ()) with
-      input_feeder = Input_feeder.from_list !inputs;
+      input_feeder = Dj_common.Input_feeder.from_list !inputs;
     }
   in
   try Interpreter.eval session program with
   | Interpreter.Terminate v -> Format.printf "%a" Interpreter.pp_dvalue v
   | ex -> raise ex
 
-
-(* Run concolic tester: *)
-let rec test_program_concolic source counter =
-  if counter <= 0 then Format.printf "Timeout limit reached. Program will not terminate...\n" else 
-  let program = Dj_common.File_utils.read_source source in 
-  try Concolic2.concolic_eval program with
-  | Concolic_exceptions.All_Branches_Hit -> Format.printf "All branches hit.\n"
-  | Concolic_exceptions.Unreachable_Branch(b) -> Format.printf "Unreachable branch: %s\n" (Ast_branch.to_string b)
-  | Concolic_exceptions.Unsatisfiable_Branch(b) -> Format.printf "Unsatisfiable branch: %s\n" (Ast_branch.to_string b)
-  | Concolic_exceptions.Reach_max_step(_, _) -> Format.printf "Reach max step... re-evaluating:\n"; test_program_concolic2 source (counter - 1)
-  | ex -> raise ex 
-
-let speclist = 
-  [("-i", Arg.Set_string source_file, "Input source file");
-   ("-m", Arg.Set_string mode, "Interpreter mode")]
-
-let reset_limit = 20
+let () =
+  Arg.parse
+    [ ("-i", Arg.Set_string source_file, "Iutput source file") ]
+    anon_fun usage_msg ;
+  run_program !source_file
 
 let () = 
   Arg.parse speclist anon_fun usage_msg;
