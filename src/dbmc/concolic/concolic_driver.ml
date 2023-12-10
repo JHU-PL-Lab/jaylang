4517668747c38b89b307f3ee17adc8341ca30dca
brandonzstride <brandonzstride@hotmail.com>
2023-12-10 15:34:11 -0500

    restore jil.exe

--- src/dbmc/concolic/concolic_driver.ml
+++ src/dbmc/concolic/concolic_driver.ml
@@ -9,9 +9,9 @@ let rec test_program_concolic source counter =
     try Concolic.concolic_eval program with
     | Concolic_exceptions.All_Branches_Hit -> Format.printf "All branches hit.\n"
     | Concolic_exceptions.Unreachable_Branch b ->
-        Format.printf "Unreachable branch: %s\n" (Concolic.branch_to_str b)
+        Format.printf "Unreachable branch: %s\n" (Ast_branch.to_string b)
     | Concolic_exceptions.Unsatisfiable_Branch b ->
-        Format.printf "Unsatisfiable branch: %s\n" (Concolic.branch_to_str b)
+        Format.printf "Unsatisfiable branch: %s\n" (Ast_branch.to_string b)
     | Concolic_exceptions.Reach_max_step (_, _) ->
         Format.printf "Reach max step... re-evaluating:\n" ;
         test_program_concolic source (counter - 1)
