4471f04dc8aeb0314ea05e3d963215c1c8900630
brandonzstride <brandonzstride@hotmail.com>
2023-12-26 20:33:42 -0800

    no msg

--- src/dbmc/concolic/concolic_driver.ml
+++ src/dbmc/concolic/concolic_driver.ml
@@ -1,26 +1,8 @@
+(*
+  See src-test/dbmc/test_concolic.ml
+*)
+
 open Core
-open Dbmc
-
-let usage_msg = "jil -i <file> [<input_i>]"
-let source_file = ref ""
-let inputs = ref []
-
-let anon_fun i_raw =
-  let this_i = int_of_string_opt i_raw in
-  inputs := !inputs @ [ this_i ]
-
-let run_program source =
-  let program = Dj_common.File_utils.read_source source in
-  let session =
-    {
-      (Interpreter.make_default_session ()) with
-      input_feeder = Dj_common.Input_feeder.from_list !inputs;
-    }
-  in
-  try Interpreter.eval session program with
-  | Interpreter.Terminate v -> Format.printf "%a" Interpreter.pp_dvalue v
-  | ex -> raise ex
-
 
 (* Run concolic tester: *)
 let rec test_program_concolic source counter =
@@ -32,12 +14,3 @@ let rec test_program_concolic source counter =
   | Concolic_exceptions.Unsatisfiable_Branch(b) -> Format.printf "Unsatisfiable branch: %s\n" (Branch.Ast_branch.to_string b)
   | Concolic_exceptions.Reach_max_step(_, _) -> Format.printf "Reach max step... re-evaluating:\n"; test_program_concolic source (counter - 1)
   | ex -> raise ex 
-
-let speclist = 
-  [("-i", Arg.Set_string source_file, "Input source file")]
-
-let reset_limit = 20
-
-let () = 
-  Arg.parse speclist anon_fun usage_msg;
-  test_program_concolic !source_file reset_limit
