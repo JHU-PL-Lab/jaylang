275603e26147680496f146489608258a3af4d635
brandonzstride <brandonzstride@hotmail.com>
2024-01-03 14:23:40 -0500

    fix expected test result

--- test/dbmc/concolic/aborts/implied_double_abort.expect.s
+++ test/dbmc/concolic/aborts/implied_double_abort.expect.s
@@ -1,6 +1,6 @@
 (
   (y_is_x_branch Found_abort Hit)
   (y_is_not_x_branch Found_abort Unsatisfiable)
-  (x_gt_neg_5_branch Hit Hit)
+  (x_gt_neg_11_branch Hit Hit)
   (last_branch Unsatisfiable Hit)
 )
