44ffedb826da6c858f3c50f7756c2fbf81b7aa12
brandonzstride <brandonzstride@hotmail.com>
2024-01-07 23:54:12 -0500

    merge branch_tracker branch

--- src/dbmc/concolic/branch.mli
+++ src/dbmc/concolic/branch.mli
@@ -1,25 +1,5 @@
 open Jayil
 
-(* module Status :
-  sig
-    module Input :
-      sig
-        type t = Lookup_key.t * Dvalue.t
-      end
-
-    type t =
-      | Hit of Input.t list
-      | Unhit
-      | Unsatisfiable (* TODO: payload? *)
-      | Found_abort of Input.t list
-      | Reach_max_step of int (* counter for how many times *)
-      | Missed
-      | Unreachable_because_aborts of 
-      | Unreachable (* Only to be used when conveying final results. No branch is ever unreachable during evaluation *)
-
-    val to_string : t -> string
-  end *)
-
 module Direction :
   sig
     type t =
@@ -91,46 +71,3 @@ module Runtime :
     val print_target_option : t option -> unit
     (** [print_target_option x] prints the branch [x] as "target branch", or prints it as "None". *)
   end
-
-(*
-  A `Status_store` tracks how AST branches are hit. It maps branch identifiers to their status.
-*)
-(* module Status_store :
-  sig
-    type t [@@deriving sexp, compare]
-    (** [t] is a map from a branch identifier to the status of the branch. So it tells
-        us whether the true and false direction of each branch have been hit. *)
-
-    val empty : t
-    (** [empty] has no information on any branches *)
-
-    val print : t -> unit
-    (** [print store] prints the statuses of all branches in the [store] to stdout. *)
-
-    val add_branch_id : t -> Ast.ident -> t
-    (** [add_branch_id store id] is a new store where the identifier [id] has been added to
-        the branch store [store], and both directions of the new branch are unhit. See
-        [Branch_status.both_unhit]. *)
-
-    val get_unhit_branch : t -> T.t option
-    (** [get_unhit_branch store] is some branch that is unhit. *)
-
-    val set_branch_status : new_status:Status.t -> t -> T.t -> t
-    (** [set_branch_status status store branch] is a new store where the given [branch] now has the
-        [status]. All other branches are unaffected. *)
-
-    val is_hit : t -> T.t -> bool
-    (** [is_hit store branch] is true if and only if the status of [branch.branch_ident] in 
-        the [store] has [branch.direction] as [Hit]. *)
-
-    val get_status : t -> T.t -> Status.t
-    (** [get_status store branch] is the status of the given [branch]. *)
-
-    val find_branches : Ast.expr -> t -> t
-    (** [find_branches e store] is a new store where all the branches in the given expression [expr]
-        have been added as unhit branches to the given [store]. *)
-
-    val finish : t -> t
-    (** [finish store] is a new store where all unhit branches are now marked as unsatisfiable *)
-
-  end *)
