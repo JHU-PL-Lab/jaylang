016182418a4f7734dc82b9201f0e97ed39d590b7
Brandon Stride <brandonzstride@hotmail.com>
2024-02-12 11:53:32 -0500

    tree mli

--- src/dbmc/concolic/strict/tree.ml
+++ src/dbmc/concolic/strict/tree.ml
@@ -1,10 +1,23 @@
 open Core
 
-module rec Node : (* serves as root node *)
+(*
+  -----------------
+  MODULE SIGNATURES   
+  -----------------
+
+  We have lowercase type names for all types in recursive modules. We use destructive
+  substitution to fill the types in when they're actually used as module signatures in the
+  recursive modules.
+*)
+
+module type NODE =
   sig
+    type children
+    type child
+    type status
     type t =
       { formulas : Formula_set.t
-      ; children : Children.t } [@@deriving compare]
+      ; children : children } [@@deriving compare]
     (** [t] is the root of the JIL program. *)
     val empty : t
     (** [empty] is the tree before the program has ever been run. It has no formulas or children. *)
@@ -13,18 +26,94 @@ module rec Node : (* serves as root node *)
     (** [merge a b] combines the trees [a] and [b] and throws an exception if there is a discrepancy. *)
     val add_formula : t -> Z3.Expr.expr -> t
     (** [add_formulas t expr] is [t] that has gained [expr] as a formula. *)
-    val get_child : t -> Branch.Runtime.t -> Child.t option
+    val get_child : t -> Branch.Runtime.t -> child option
     (** [get_child t branch] is the child of [t] by taking the [branch], if it exists. *)
-    val get_child_exn : t -> Branch.Runtime.t -> Child.t
+    val get_child_exn : t -> Branch.Runtime.t -> child
     (** [get_child_exn t branch] is the child of [t] by taking the [branch], or exception. *)
     val is_valid_target_child : t -> Branch.Runtime.t -> bool
     (** [is_valid_target t branch] is [true] if and only if [branch] should be a target child from [t]. *)
     val with_formulas : t -> Formula_set.t -> t
     (** [with_formulas t formulas] is [t] with the given [formulas] overwriting its old formulas. *)
-    val set_status : t -> Child.t -> Status.t -> Path.t -> t
+    val set_status : t -> child -> status -> Path.t -> t
     (** [set_status t child status path] is [t] where [child] is given [status], and [child] is necessarily
         found along the [path]. *)
   end
+
+module type CHILDREN = 
+  sig
+    type node
+    type child
+    type t [@@deriving compare]
+    (** [t] represents the branches underneath some node. *)
+    val empty : t
+    (** [empty] is no children. *)
+    (* val is_empty : t -> bool *)
+    val set_node : t -> Branch.Runtime.t -> node -> t
+    (** [add t branch child] adds [child] as a node underneath the [branch] in [t]. *)
+    val merge : t -> t -> t
+    (** [merge a b] merges all children in [a] and [b]. *)
+    val get_child : t -> Branch.Runtime.t -> child option
+    (** [get_child t branch] is the child in [t] by taking the [branch]. *)
+    val is_valid_target : t -> Branch.Runtime.t -> bool
+    (** [is_valid_target t branch] is [true] if and only if [branch] should be a target. *)
+    val set_child : t -> child -> t
+  end
+
+module type CHILD =
+  sig
+    type status
+    type node
+    type t =
+      { status      : status
+      ; constraints : Formula_set.t
+      ; branch      : Branch.Runtime.t
+      } [@@deriving compare]
+    (** [t] is a single child of a [Node.t] *)
+    val create : node -> Branch.Runtime.t -> t
+    (** [create node branch] makes a child by taken the [branch] to reach the given [node]. *)
+    val create_both : node -> Branch.Runtime.t -> t * t
+    (** [create_both node branch] makes a child by taking the [branch] to reach the given [node]. 
+        Also returns the other side as unsolved. **Note** [node] is the child, not the parent. *)
+    val merge : t -> t -> t
+    val is_valid_target : t -> bool
+    val to_node_exn : t -> node
+    val unsolved : Branch.Runtime.t -> t
+    val to_formulas : t -> Z3.Expr.expr list
+    val map_node : t -> f:(node -> node) -> t
+  end
+  
+module type STATUS =
+  sig
+    type node
+    type t =
+      | Hit of node
+      | Unsatisfiable
+      | Unknown (* for timeouts *)
+      | Unsolved (* not yet tried *)
+      [@@deriving compare]
+    (** [t] is a node during a solve. It has been hit, determined unsatisfiable,
+        is not known if hittable or unsatisfiable, or has not been solved or seen yet.
+        Unsatisfiable or Unknown nodes are status of the node before they've ever been
+        hit during interpretation, so there is no existing node as a payload. *)
+      
+    val merge : t -> t -> t
+    (** [merge a b] keeps the most information from [a] or [b] and merges the nodes if both are [Hit]. *)
+
+    val is_valid_target : t -> bool
+  end
+
+
+(*
+  --------------------------------
+  RECURSIVE MODULE IMPLEMENTATIONS   
+  --------------------------------
+*)
+
+module rec Node : (* serves as root node *)
+  NODE with
+  type children := Children.t and
+  type child := Child.t and
+  type status := Status.t 
   =
   struct
     type t =
@@ -77,22 +166,9 @@ module rec Node : (* serves as root node *)
 
   end
 and Children :
-  sig
-    type t [@@deriving compare]
-    (** [t] represents the branches underneath some node. *)
-    val empty : t
-    (** [empty] is no children. *)
-    (* val is_empty : t -> bool *)
-    val set_node : t -> Branch.Runtime.t -> Node.t -> t
-    (** [add t branch child] adds [child] as a node underneath the [branch] in [t]. *)
-    val merge : t -> t -> t
-    (** [merge a b] merges all children in [a] and [b]. *)
-    val get_child : t -> Branch.Runtime.t -> Child.t option
-    (** [get_child t branch] is the child in [t] by taking the [branch]. *)
-    val is_valid_target : t -> Branch.Runtime.t -> bool
-    (** [is_valid_target t branch] is [true] if and only if [branch] should be a target. *)
-    val set_child : t -> Child.t -> t
-  end
+  CHILDREN with
+  type node := Node.t and
+  type child := Child.t
   =
   struct
     type t = 
@@ -160,25 +236,9 @@ and Children :
       | No_children, _ -> true (* no children *) 
   end
 and Child : 
-  sig
-    type t =
-      { status      : Status.t
-      ; constraints : Formula_set.t
-      ; branch      : Branch.Runtime.t
-      } [@@deriving compare]
-    (** [t] is a single child of a [Node.t] *)
-    val create : Node.t -> Branch.Runtime.t -> t
-    (** [create node branch] makes a child by taken the [branch] to reach the given [node]. *)
-    val create_both : Node.t -> Branch.Runtime.t -> t * t
-    (** [create_both node branch] makes a child by taking the [branch] to reach the given [node]. 
-        Also returns the other side as unsolved. **Note** [node] is the child, not the parent. *)
-    val merge : t -> t -> t
-    val is_valid_target : t -> bool
-    val to_node_exn : t -> Node.t
-    val unsolved : Branch.Runtime.t -> t
-    val to_formulas : t -> Z3.Expr.expr list
-    val map_node : t -> f:(Node.t -> Node.t) -> t
-  end
+  CHILD with
+  type status := Status.t and
+  type node := Node.t
   =
   struct
     type t =
@@ -237,23 +297,8 @@ and Child :
       | _ -> x
   end
 and Status :
-  sig
-    type t =
-      | Hit of Node.t
-      | Unsatisfiable
-      | Unknown (* for timeouts *)
-      | Unsolved (* not yet tried *)
-      [@@deriving compare]
-    (** [t] is a node during a solve. It has been hit, determined unsatisfiable,
-        is not known if hittable or unsatisfiable, or has not been solved or seen yet.
-        Unsatisfiable or Unknown nodes are status of the node before they've ever been
-        hit during interpretation, so there is no existing node as a payload. *)
-      
-    val merge : t -> t -> t
-    (** [merge a b] keeps the most information from [a] or [b] and merges the nodes if both are [Hit]. *)
-
-    val is_valid_target : t -> bool
-  end
+  STATUS with
+  type node := Node.t
   =
   struct
     type t =
---
+++ src/dbmc/concolic/strict/tree.mli
@@ -0,0 +1,83 @@
+
+module rec Node :
+  sig
+    type t =
+      { formulas : Formula_set.t
+      ; children : Children.t } [@@deriving compare]
+    (** [t] is the root of the JIL program. *)
+    val empty : t
+    (** [empty] is the tree before the program has ever been run. It has no formulas or children. *)
+    (* val add_child : t -> Branch.Runtime.t -> t *)
+    val merge : t -> t -> t
+    (** [merge a b] combines the trees [a] and [b] and throws an exception if there is a discrepancy. *)
+    val add_formula : t -> Z3.Expr.expr -> t
+    (** [add_formulas t expr] is [t] that has gained [expr] as a formula. *)
+    val get_child : t -> Branch.Runtime.t -> Child.t option
+    (** [get_child t branch] is the child of [t] by taking the [branch], if it exists. *)
+    val get_child_exn : t -> Branch.Runtime.t -> Child.t
+    (** [get_child_exn t branch] is the child of [t] by taking the [branch], or exception. *)
+    val is_valid_target_child : t -> Branch.Runtime.t -> bool
+    (** [is_valid_target t branch] is [true] if and only if [branch] should be a target child from [t]. *)
+    val with_formulas : t -> Formula_set.t -> t
+    (** [with_formulas t formulas] is [t] with the given [formulas] overwriting its old formulas. *)
+    val set_status : t -> Child.t -> Status.t -> Path.t -> t
+    (** [set_status t child status path] is [t] where [child] is given [status], and [child] is necessarily
+        found along the [path]. *)
+  end
+and Children :
+  sig
+    type t [@@deriving compare]
+    (** [t] represents the branches underneath some node. *)
+    val empty : t
+    (** [empty] is no children. *)
+    (* val is_empty : t -> bool *)
+    val set_node : t -> Branch.Runtime.t -> Node.t -> t
+    (** [add t branch child] adds [child] as a node underneath the [branch] in [t]. *)
+    val merge : t -> t -> t
+    (** [merge a b] merges all children in [a] and [b]. *)
+    val get_child : t -> Branch.Runtime.t -> Child.t option
+    (** [get_child t branch] is the child in [t] by taking the [branch]. *)
+    val is_valid_target : t -> Branch.Runtime.t -> bool
+    (** [is_valid_target t branch] is [true] if and only if [branch] should be a target. *)
+    val set_child : t -> Child.t -> t
+  end
+and Child :
+  sig
+    type t =
+      { status      : Status.t
+      ; constraints : Formula_set.t
+      ; branch      : Branch.Runtime.t
+      } [@@deriving compare]
+    (** [t] is a single child of a [Node.t] *)
+    val create : Node.t -> Branch.Runtime.t -> t
+    (** [create node branch] makes a child by taken the [branch] to reach the given [node]. *)
+    val create_both : Node.t -> Branch.Runtime.t -> t * t
+    (** [create_both node branch] makes a child by taking the [branch] to reach the given [node]. 
+        Also returns the other side as unsolved. **Note** [node] is the child, not the parent. *)
+    val merge : t -> t -> t
+    val is_valid_target : t -> bool
+    val to_node_exn : t -> Node.t
+    val unsolved : Branch.Runtime.t -> t
+    val to_formulas : t -> Z3.Expr.expr list
+    val map_node : t -> f:(Node.t -> Node.t) -> t
+  end
+and Status :
+  sig
+    type t =
+      | Hit of Node.t
+      | Unsatisfiable
+      | Unknown (* for timeouts *)
+      | Unsolved (* not yet tried *)
+      [@@deriving compare]
+    (** [t] is a node during a solve. It has been hit, determined unsatisfiable,
+        is not known if hittable or unsatisfiable, or has not been solved or seen yet.
+        Unsatisfiable or Unknown nodes are status of the node before they've ever been
+        hit during interpretation, so there is no existing node as a payload. *)
+      
+    val merge : t -> t -> t
+    (** [merge a b] keeps the most information from [a] or [b] and merges the nodes if both are [Hit]. *)
+
+    val is_valid_target : t -> bool
+  end
+
+module Root = Node (* for better naming *)
