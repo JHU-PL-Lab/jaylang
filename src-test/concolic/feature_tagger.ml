7951be589c3cac7790a7dd2baff5c7ab8e7d23ca
Brandon Stride <brandonzstride@hotmail.com>
2024-07-11 16:51:48 -0700

    no msg

--- src-test/concolic/feature_tagger.ml
+++ src-test/concolic/feature_tagger.ml
@@ -9,6 +9,10 @@
 
 open Core
 
+[@@@ocaml.warning "-32"]
+[@@@ocaml.warning "-37"]
+[@@@ocaml.warning "-34"]
+
 module Bjy_comments =
   struct
     let read_file (filename : Filename.t) : string list option =
@@ -21,6 +25,65 @@ module Bjy_comments =
   end
 
 module Tags =
+  struct
+
+    module Status =
+      struct
+        type t = Checked | Unchecked | Missing [@@deriving compare]
+
+        let t_of_sexp sexp =
+          match String.t_of_sexp sexp with
+          | "" -> Unchecked
+          | "x" -> Checked
+          | _ -> Missing
+        
+        let sexp_of_t x =
+          begin
+          match x with
+          | Checked -> "x"
+          | Unchecked -> ""
+          | Missing -> "TODO"
+          end
+          |> String.sexp_of_t
+      end
+
+    type t =
+      { polymorphic_types      : Status.t [@sexplib.default ""]
+      ; variants               : Status.t
+      ; intersection_types     : Status.t
+      ; recursive_functions    : Status.t
+      ; mu_types               : Status.t
+      ; higher_order_functions : Status.t
+      ; subtyping              : Status.t
+      ; type_casing            : Status.t
+      ; oop_style              : Status.t
+      ; refinement_types       : Status.t
+      ; dependent_types        : Status.t
+      ; parametric_types       : Status.t
+      ; records                : Status.t
+      ; trees                  : Status.t
+      ; wrap_required          : Status.t }
+      [@@deriving sexp, compare]
+
+    let default =
+      { polymorphic_types      = Checked
+      ; variants               = Checked
+      ; intersection_types     = Checked
+      ; recursive_functions    = Missing
+      ; mu_types               = Checked
+      ; higher_order_functions = Checked
+      ; subtyping              = Unchecked
+      ; type_casing            = Checked
+      ; oop_style              = Checked
+      ; refinement_types       = Checked
+      ; dependent_types        = Checked
+      ; parametric_types       = Checked
+      ; records                = Checked
+      ; trees                  = Checked
+      ; wrap_required          = Checked }
+  end
+
+module Tag =
   struct
     type t =
       | Polymorphic_types
@@ -106,7 +169,8 @@ module Tags =
   end
 
 let () =
-  Format.printf "%s\n" Tags.show_all;
+  Format.printf "%s\n" (Tags.sexp_of_t Tags.default |> Sexp.to_string_hum ~indent:2)
+  (* Format.printf "%s\n" Tag.show_all;
   match Sys.get_argv () |> List.of_array with
   | _ :: filename :: [] -> begin
     match Bjy_comments.read_file filename with
@@ -114,8 +178,8 @@ let () =
     | Some lines ->
       List.iter lines ~f:(fun line ->
         let tag_opt_string, is_checked =
-          Tags.read_checked line
-          |> Tuple2.map_fst ~f:(function None -> "none" | Some tag -> Tags.show tag)
+          Tag.read_checked line
+          |> Tuple2.map_fst ~f:(function None -> "none" | Some tag -> Tag.show tag)
         in
         Format.printf
           "Line is '%s', tag is '%s', checked is %b\n"
@@ -124,7 +188,7 @@ let () =
           is_checked
       )
   end
-  | _ -> Format.eprintf "Bad arguments\n"
+  | _ -> Format.eprintf "Bad arguments\n" *)
   
 
 
