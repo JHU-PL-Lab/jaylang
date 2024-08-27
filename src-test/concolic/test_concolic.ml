7fb41a479faca17c02d3e3e6243640d1a9ea00c7
Brandon Stride <brandonzstride@hotmail.com>
2024-08-27 18:59:40 -0400

    reveal well-typed tests again

--- src-test/concolic/test_concolic.ml
+++ src-test/concolic/test_concolic.ml
@@ -51,18 +51,18 @@ let () =
       (* @ make_tests_ill_typed "bjy/deep-type-error" *)
 
       @ make_tests_ill_typed "bjy/buggy-ill-typed"
-      (* @ make_tests_well_typed "bjy/buggy-well-typed" *)
+      @ make_tests_well_typed "bjy/buggy-well-typed"
 
       @ make_tests_ill_typed "bjy/oopsla-24-tests-ill-typed"
-      (* @ make_tests_well_typed "bjy/oopsla-24-tests-well-typed" *)
+      @ make_tests_well_typed "bjy/oopsla-24-tests-well-typed"
 
       @ make_tests_ill_typed "bjy/oopsla-24-benchmarks-ill-typed"
-      (* @ make_tests_well_typed "bjy/oopsla-24-benchmarks-well-typed" *)
+      @ make_tests_well_typed "bjy/oopsla-24-benchmarks-well-typed"
 
       @ make_tests_ill_typed "bjy/scheme-pldi-2015-ill-typed"
-      (* @ make_tests_well_typed "bjy/scheme-pldi-2015-well-typed" *)
+      @ make_tests_well_typed "bjy/scheme-pldi-2015-well-typed"
 
       @ make_tests_ill_typed "bjy/sato-bjy-ill-typed"
-      (* @ make_tests_well_typed "bjy/sato-bjy-well-typed" *)
+      @ make_tests_well_typed "bjy/sato-bjy-well-typed"
     ) 
     ~quick_only:false
