495f87b7e6c0e913826050c4165e25dabea3cb5c
Brandon Stride <brandonzstride@hotmail.com>
2024-09-29 10:56:42 -0400

    perf: slightly better target compare

--- src/concolic/structure/target.ml
+++ src/concolic/structure/target.ml
@@ -1,15 +1,18 @@
 open Core
 
 type t =
-  { branch : Branch.Runtime.t [@compare.ignore] (* is included in the path, so we don't need to compare *)
-  ; path_n : int [@compare.ignore] (* this is the length of the path, just so we don't recompute *)
+  { branch : Branch.Runtime.t (* is included in the path *)
+  ; path_n : int (* this is the length of the path, just so we don't recompute *)
   ; path   : Path.t } (* The path just helps the solver find the node in the tree in order to gather formulas *)
-  [@@deriving compare]  
   (* The path is really the only thing that describes the target completely: a series of left and right branch choices *)
 
 let compare (a : t) (b : t) : int =
   match Int.compare a.path_n b.path_n with
-  | 0 -> compare a b (* call the derived compare *)
+  | 0 -> begin
+    match Branch.Runtime.compare a.branch b.branch with
+    | 0 -> Path.compare a.path b.path
+    | x -> x
+  end 
   | x -> x
 
 let create (branch : Branch.Runtime.t) (path : Path.t) : t =
--- src/concolic/structure/target.mli
+++ src/concolic/structure/target.mli
@@ -6,7 +6,7 @@ type t =
   (* The path is really the only thing that describes the target completely: a series of left and right branch choices *)
 
 val compare : t -> t -> int
-(** [compare a b] comapres the paths in the targets [a] and [b]. *)
+(** [compare a b] compares the paths in the targets [a] and [b], and it preliminarily uses the branch and path length to weed out similar paths. *)
 
 val create : Branch.Runtime.t -> Path.t -> t
 (** [create branch path] is a target for the [branch] and the end of the [path], where [branch] is included as the last
