7fc01ed7f18cd9384f415756942e5ecdee7a0e98
Brandon Stride <brandonzstride@hotmail.com>
2024-10-15 16:39:52 -0400

    refactor: claim to expression reduce duplicate code

--- src/concolic/solve/claim.ml
+++ src/concolic/solve/claim.ml
@@ -7,16 +7,13 @@ type t =
 
 let[@landmarks] get_formulas (claims : t list) (cache : Expression.Cache.t) : bool C_sudu.Gexpr.t list =
   List.fold claims ~init:[] ~f:(fun acc claim ->
-    let open Expression in
+    let accum mk_key lookup key e acc =
+      let key_expr = Expression.t_to_formula @@ mk_key key in
+      C_sudu.eq key_expr (Expression.t_to_formula e)
+      :: C_sudu.eq key_expr (Expression.t_to_formula @@ lookup cache key)
+      :: acc
+    in
     match claim with
-    | Int_equality (key, i_expr) ->
-      let g_expr = t_to_formula i_expr in
-      let key_expr = t_to_formula @@ Expression.int_key key in
-      let expr = t_to_formula @@ Expression.Cache.lookup_int cache key in
-      C_sudu.eq key_expr g_expr :: C_sudu.eq key_expr expr :: acc
-    | Bool_equality (key, b_expr) ->
-      let g_expr = t_to_formula b_expr in
-      let key_expr = t_to_formula @@ Expression.bool_key key in
-      let expr = t_to_formula @@ Expression.Cache.lookup_bool cache key in
-      C_sudu.eq key_expr g_expr :: C_sudu.eq key_expr expr :: acc
+    | Int_equality (key, i_expr) -> accum Expression.int_key Expression.Cache.lookup_int key i_expr acc
+    | Bool_equality (key, b_expr) -> accum Expression.bool_key Expression.Cache.lookup_bool key b_expr acc
     )
