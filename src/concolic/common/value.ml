e25090f4210f734f9d27d9bf489bb9d2a44f4f7d
Brandon Stride <brandonzstride@hotmail.com>
2025-03-10 09:28:35 -0400

    preliminary implementation

--- src/concolic/common/value.ml
+++ src/concolic/common/value.ml
@@ -1,4 +1,85 @@
 
-include Lang.Value.Embedded (struct type 'a t = 'a * 'a Expression.t let to_string f (v, _) = f v end)
+open Core
+open Lang
+open Ast
 
-include T
+exception UnboundVariable of Ident.t
+
+module type V = sig
+  type 'a t
+  val to_string : ('a -> string) -> 'a t -> string
+end
+
+module Tbl = Hashtbl.Make (Ident)
+
+(*
+  V is the payload of int and bool. We do this so that we can
+  inject Z3 expressions into the values of the concolic evaluator.
+*)
+module Make (V : V) = struct
+  module T = struct
+    type t =
+      (* all languages *)
+      | VInt of int V.t
+      | VBool of bool V.t
+      | VFunClosure of { param : Ident.t ; body : closure }
+      | VVariant of { label : VariantLabel.t ; payload : t }
+      | VRecord of t RecordLabel.Map.t
+      | VTypeMismatch
+      | VAbort
+      | VDiverge
+      (* embedded only *)
+      | VId
+      | VFrozen of closure
+
+    and env = t Store.Ref.t Tbl.t
+
+    and closure = { expr : Constraints.embedded Expr.t ; snap : Store.snapshot } (* an expression to be evaluated in an environment *)
+  end
+
+  include T
+
+  let rec to_string : t -> string = function
+    | VInt i -> V.to_string Int.to_string i
+    | VBool b -> V.to_string Bool.to_string b
+    | VFunClosure { param = Ident s ; _ } -> Format.sprintf "(fun %s -> <expr>)" s
+    | VVariant { label ; payload } -> Format.sprintf "(`%s (%s))" (VariantLabel.to_string label) (to_string payload)
+    | VRecord record_body -> RecordLabel.record_body_to_string ~sep:"=" record_body to_string
+    | VTypeMismatch -> "Type_mismatch"
+    | VAbort -> "Abort"
+    | VDiverge -> "Diverge"
+    | VId -> "(fun x -> x)"
+    | VFrozen _ -> "(Freeze <expr>)"
+
+  module Env = struct
+    type t = env
+    let env : t = Hashtbl.create (module Ident)
+    let store = Store.create ()
+
+    (* let snapshots = Stack.create () *)
+
+    (* let ref_is_changed = ref false *)
+    (* let ref_waiting_to_snapback = ref false *)
+
+    let fetch id = 
+      (* if !ref_waiting_to_snapback && !ref_is_changed
+      then begin
+        let snap = Stack.pop_exn snapshots in
+        Store.restore store snap;
+      end *)
+      Store.Ref.get store
+      @@ Hashtbl.find_exn env id
+
+    let add id v =
+      match Hashtbl.find env id with
+      | Some cell -> 
+        (* ref_is_changed := true; *)
+        Store.Ref.set store cell v
+      | None ->
+        Hashtbl.set env ~key:id ~data:(
+          Store.Ref.make store v
+        )
+  end
+end
+
+include Make (struct type 'a t = 'a * 'a Expression.t let to_string f (v, _) = f v end)
--- src/concolic/evaluator.ml
+++ src/concolic/evaluator.ml
@@ -4,62 +4,6 @@ open Lang.Ast
 open Expr
 open Value
 
-module State_M = struct
-  module State = struct
-    type t = { step : int ; session : Eval_session.t }
-  end
-
-  include Utils.State_read_result.Make (State) (Env) (Status.Eval)
-
-  let run (x : 'a m) (session : Eval_session.t) : Status.Eval.t =
-    x.run
-      ~reject:Fn.id
-      ~accept:(fun _ s -> Eval_session.finish s.session)
-      State.{ step = 0 ; session }
-      Env.empty
-
-  let[@inline always] modify_session (f : Eval_session.t -> Eval_session.t) : unit m =
-    modify (fun s -> { s with session = f s.session })
-
-  (*
-    As a general observation, it is more efficient to be hands-on and write this
-    using the structure of FMonad instead of using `read`, `return`, etc.
-    I suppose this is due to better inlining. I consider the tradeoff in favor
-    of more efficiency worth the less general code.
-  *)
-  let incr_step : int m =
-    { run =
-      fun ~reject ~accept s _ ->
-        let step = s.step + 1 in
-        if step > Eval_session.get_max_step s.session
-        then reject @@ Eval_session.reach_max_step s.session
-        else accept step { s with step }
-    }
-
-  let[@inline always] modify_and_return (f : Eval_session.t -> Eval_session.t * 'a) : 'a m =
-    { run =
-      fun ~reject:_ ~accept s _ ->
-        let session, a = f s.session in
-        accept a { s with session }
-    }
-
-  let read_env : Env.t m =
-    let%bind (_, e) = read in
-    return e
-
-  let[@inline always] fetch (id : Ident.t) : Value.t m =
-    let%bind env = read_env in
-    return @@ Env.fetch id env
-
-  let[@inline always] fail_map (f : Eval_session.t -> Status.Eval.t) : 'a m =
-    let%bind ({ session ; _ }, _) = read in
-    fail @@ f session
-
-  let abort : 'a m = fail_map Eval_session.abort
-  let diverge : 'a m = fail_map Eval_session.diverge
-  let type_mismatch s : 'a m = fail_map @@ Fn.flip Eval_session.type_mismatch s
-end
-
 module Error_msg = struct
   let project_non_record label v =
     Format.sprintf "Label %s not found in non-record `%s`" (RecordLabel.to_string label) (Value.to_string v)
@@ -94,6 +38,8 @@ module Error_msg = struct
     Format.sprintf "Case on non-int `%s`" (Value.to_string v)
 end
 
+exception Failed_interp of Status.Eval.t
+
 (*
   The only time we really update the session is on max step or hitting a branch.
   Can I then optimize it to not have to pass it around most of the time?
@@ -103,90 +49,109 @@ let eval_exp
   (expr : Embedded.t) 
   : Status.Eval.t
   =
-  let open State_M in
+  let ref_sess = ref session in
+  let ref_step = ref 0 in
+  let max_step = Eval_session.get_max_step !ref_sess in
 
-  let rec eval (expr : Embedded.t) : Value.t m =
-    let open Value.M in (* puts the value constructors in scope *)
-    let%bind step = incr_step in
+  let type_mismatch s = 
+    raise @@ Failed_interp (Eval_session.type_mismatch !ref_sess s) in
+  
+  (* let open State_M in *)
+
+  let rec eval (expr : Embedded.t) : Value.t =
+    incr ref_step;
+    if !ref_step > max_step 
+    then raise @@ Failed_interp (Eval_session.reach_max_step !ref_sess);
     match expr with
     (* Ints and bools -- constant expressions *)
-    | EInt i -> return @@ VInt (i, Expression.const_int i)
-    | EBool b -> return @@ VBool (b, Expression.const_bool b)
+    | EInt i -> VInt (i, Expression.const_int i)
+    | EBool b -> VBool (b, Expression.const_bool b)
     (* Simple -- no different than interpreter *)
-    | EVar id -> fetch id
+    | EVar id -> Env.fetch id
     | EFunction { param ; body } ->
-      let%bind env = read_env in
-      return @@ VFunClosure { param ; body = { expr = body ; env } }
-    | EId -> return VId
+      let snap = Store.capture Env.store in
+      VFunClosure { param ; body = { expr = body ; snap } } (* TODO: use a snapshot here *)
+    | EId -> VId
     | EFreeze e_freeze_body -> 
-      let%bind env = read_env in
-      return @@ VFrozen { expr = e_freeze_body ; env }
+      let snap = Store.capture Env.store in
+      VFrozen { expr = e_freeze_body ; snap }
     | EVariant { label ; payload = e_payload } -> 
-      let%bind payload = eval e_payload in
-      return @@ VVariant { label ; payload }
+      let payload = eval e_payload in
+       VVariant { label ; payload }
     | EProject { record = e_record ; label } -> begin
-      let%bind v = eval e_record in
+      let v = eval e_record in
       match v with
       | VRecord record_body -> begin
         match Map.find record_body label with
-        | Some v -> return v
-        | None -> type_mismatch @@ Error_msg.project_missing_label label v
+        | Some v -> v
+        | None -> raise @@ Failed_interp (type_mismatch @@ Error_msg.project_missing_label label v)
       end
       | _ -> type_mismatch @@ Error_msg.project_non_record label v
     end
     | EThaw e_frozen -> begin
-      let%bind v = eval e_frozen in
+      let v = eval e_frozen in
       match v with
-      | VFrozen { expr ; env } -> local (fun _ -> env) (eval expr)
+      | VFrozen { expr ; snap } -> 
+        let snap' = Store.capture Env.store in
+        Store.restore Env.store snap;
+        let res = eval expr in
+        Store.restore Env.store snap';
+        res
       | _ -> type_mismatch @@ Error_msg.thaw_non_frozen v
     end
     | ERecord record_body ->
-      let%bind value_record_body =
-        Map.fold record_body ~init:(return RecordLabel.Map.empty) ~f:(fun ~key ~data:e acc_m ->
-          let%bind acc = acc_m in
-          let%bind v = eval e in
-          return @@ Map.set acc ~key ~data:v
+      let value_record_body =
+        Map.fold record_body ~init:RecordLabel.Map.empty ~f:(fun ~key ~data:e acc ->
+          Map.set acc ~key ~data:(eval e)
         )
       in
-      return @@ VRecord value_record_body
+      VRecord value_record_body
     | EIgnore { ignored ; cont } ->
-      let%bind _ : Value.t = eval ignored in
+      let _ : Value.t = eval ignored in
       eval cont
     | EMatch { subject ; patterns } -> begin (* Note: there cannot be symbolic branching on match *)
-      let%bind v = eval subject in
+      let v = eval subject in
       match
         (* find the matching pattern and add to env any values capture by the pattern *)
         List.find_map patterns ~f:(fun (pat, body) ->
           match pat, v with
-          | PAny, _ -> Some (body, Fn.id)
-          | PVariable id, _ -> Some (body, Env.add id v)
+          | PAny, _ -> Some body
+          | PVariable id, _ -> 
+            Env.add id v;
+            Some body
           | PVariant { variant_label ; payload_id }, VVariant { label ; payload }
               when VariantLabel.equal variant_label label ->
-            Some (body, Env.add payload_id payload)
+            Env.add payload_id payload;
+            Some body
           | _ -> None
           )
       with
-      | Some (e, f) -> local f (eval e)
+      | Some e -> eval e
       | None -> type_mismatch @@ Error_msg.pattern_not_found patterns v
     end
     | ELet { var ; body ; cont } ->
-      let%bind v = eval body in
-      local (Env.add var v) (eval cont)
+      Env.add var (eval body);
+      eval cont
     | EAppl { func ; arg } -> begin
-      let%bind vfunc = eval func in
-      let%bind varg = eval arg in
+      let vfunc = eval func in
+      let varg = eval arg in
       match vfunc with
-      | VId -> return varg
+      | VId -> varg
       | VFunClosure { param ; body } ->
-        local (fun _ -> Env.add param varg body.env) (eval body.expr)
+        let snap = Store.capture Env.store in
+        Store.restore Env.store body.snap;
+        Env.add param varg;
+        let res = eval body.expr in
+        Store.restore Env.store snap;
+        res
       | _ -> type_mismatch @@ Error_msg.bad_appl vfunc varg
     end
     (* Operations -- build new expressions *)
     | EBinop { left ; binop ; right } -> begin
-      let%bind vleft = eval left in
-      let%bind vright = eval right in
+      let vleft = eval left in
+      let vright = eval right in
       let k f e1 e2 op =
-        return @@ f (Expression.op e1 e2 op)
+        f (Expression.op e1 e2 op)
       in
       let open Expression.Typed_binop in
       let v_int n = fun e -> VInt (n, e) in
@@ -209,23 +174,23 @@ let eval_exp
       | _ -> type_mismatch @@ Error_msg.bad_binop vleft binop vright
     end
     | ENot e_not_body -> begin
-      let%bind v = eval e_not_body in
+      let v = eval e_not_body in
       match v with
-      | VBool (b, e_b) -> return @@ VBool (not b, Expression.not_ e_b) 
+      | VBool (b, e_b) -> VBool (not b, Expression.not_ e_b) 
       | _ -> type_mismatch @@ Error_msg.bad_not v
     end
     (* Branching *)
     | EIf { cond ; true_body ; false_body } -> begin
-      let%bind v = eval cond in
+      let v = eval cond in
       match v with
       | VBool (b, e) ->
         let body = if b then true_body else false_body in
-        let%bind () = modify_session @@ Eval_session.hit_branch (Direction.of_bool b) e in
+        ref_sess := Eval_session.hit_branch (Direction.of_bool b) e !ref_sess;
         eval body
       | _ -> type_mismatch @@ Error_msg.cond_non_bool v
     end
     | ECase { subject ; cases ; default } -> begin
-      let%bind v = eval subject in
+      let v = eval subject in
       let int_cases = List.map cases ~f:Tuple2.get1 in
       match v with
       | VInt (i, e) -> begin
@@ -233,23 +198,32 @@ let eval_exp
         match body_opt with
         | Some body -> (* found a matching case *)
           let other_cases = List.filter int_cases ~f:((<>) i) in
-          let%bind () = modify_session @@ Eval_session.hit_case (Direction.of_int i) e ~other_cases in
+          ref_sess := Eval_session.hit_case (Direction.of_int i) e ~other_cases !ref_sess;
           eval body
         | None -> (* no matching case, so take default case *)
-          let%bind () = modify_session @@ Eval_session.hit_case (Direction.Case_default { not_in = int_cases }) e ~other_cases:int_cases in
+          ref_sess := Eval_session.hit_case (Direction.Case_default { not_in = int_cases }) e ~other_cases:int_cases !ref_sess;
           eval default
       end
       | _ -> type_mismatch @@ Error_msg.case_non_int v
     end
     (* Inputs *)
-    | EPick_i -> modify_and_return @@ Eval_session.get_input (Stepkey.I step)
-    | EPick_b -> modify_and_return @@ Eval_session.get_input (Stepkey.B step)
+    | EPick_i -> 
+      let sess, input = Eval_session.get_input (Stepkey.I !ref_step) !ref_sess in
+      ref_sess := sess;
+      input
+    | EPick_b ->
+      let sess, input = Eval_session.get_input (Stepkey.B !ref_step) !ref_sess in
+      ref_sess := sess;
+      input
     (* Failure cases *)
-    | EAbort -> abort
-    | EDiverge -> diverge
+    | EAbort -> raise @@ Failed_interp (Eval_session.abort !ref_sess)
+    | EDiverge -> raise @@ Failed_interp (Eval_session.diverge !ref_sess)
   in
 
-  run (eval expr) session
+  try
+    let _ = eval expr in
+    Eval_session.finish !ref_sess
+  with Failed_interp status -> status
 
 (*
   -------------------
