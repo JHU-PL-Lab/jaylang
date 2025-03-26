9c1da69372d52f0d51fe434ad6333e27e0a914f3
Brandon Stride <brandonzstride@hotmail.com>
2025-03-26 19:21:19 -0400

    style: whitespace

--- benchmark/concolic/cbenchmark.ml
+++ benchmark/concolic/cbenchmark.ml
@@ -1,147 +1,144 @@
 
 open Core
 
-module Report_row (* : Latex_table.ROW *) =
-  struct
-    module Trial =
-      struct
-        type t =
-          | Number of int
-          | Average
-      end
-
+module Report_row (* : Latex_table.ROW *) = struct
+  module Trial = struct
     type t =
-      { testname          : Filename.t
-      ; test_result       : Concolic.Status.Terminal.t
-      ; time_to_interpret : Time_float.Span.t
-      ; time_to_solve     : Time_float.Span.t
-      ; total_time        : Time_float.Span.t
-      ; trial             : Trial.t
-      ; lines_of_code     : int
-      ; metadata          : Metadata.t }
-
-    let names =
-      [ "Test Name" ; "Interp" ; "Solve" ; "Total" ; "LOC" ]
-      @ (List.map Ttag.all ~f:(fun tag ->
-          Latex_format.rotate_90
-          @@ Ttag.to_string_with_underline tag
-        )
-      )
-
-    let to_strings x =
-      let span_to_ms_string =
-        fun span ->
-          let fl = Time_float.Span.to_ms span in
-          Float.to_string @@
-          if Float.(fl < 1.)
-          then Float.round_decimal fl ~decimal_digits:1
-          else Float.round_significant fl ~significant_digits:2
-      in
-      [ Filename.basename x.testname |> String.take_while ~f:(Char.(<>) '.') |> Latex_format.texttt
-      ; span_to_ms_string x.time_to_interpret
-      ; span_to_ms_string x.time_to_solve
-      ; span_to_ms_string x.total_time
-      ; Int.to_string x.lines_of_code ]
-      @ (
-        match Metadata.tags_of_t x.metadata with
-        | `Sorted_list ls ->
-          List.map ls ~f:(function
-            | `Absent -> "--"
-            | `Feature tag -> Ttag.to_string_short tag
-            | `Reason tag ->
-              Latex_format.red
-              @@ Ttag.to_string_short tag
-            )
-      )
-
-    let of_testname (n_trials : int) (testname : Filename.t) : t list =
-      assert (n_trials > 0);
-      let metadata = Metadata.of_bjy_file testname in
-      let test_one (n : int) : t =
-        let interp0 = Utils.Safe_cell.get Concolic.Evaluator.global_runtime in
-        let solve0 = Utils.Safe_cell.get Concolic.Evaluator.global_solvetime in
-        let t0 = Caml_unix.gettimeofday () in
-        let source =
-          In_channel.read_all testname
-          |> Lang.Parse.parse_single_pgm_string
-        in
-        let test_result =
-          Concolic.Driver.test_bjy source ~global_timeout_sec:90.0 ~do_wrap:true ~in_parallel:false (* parallel computation off by default *)
-        in
-        let t1 = Caml_unix.gettimeofday () in
-        let interp1 = Utils.Safe_cell.get Concolic.Evaluator.global_runtime in
-        let solve1 = Utils.Safe_cell.get Concolic.Evaluator.global_solvetime in
-        let row =
-          { testname
-          ; test_result
-          ; time_to_interpret = Time_float.Span.of_sec (interp1 -. interp0)
-          ; time_to_solve = Time_float.Span.of_sec (solve1 -. solve0)
-          ; total_time = Time_float.Span.of_sec (t1 -. t0)
-          ; trial = Number n
-          ; lines_of_code = Utils.Cloc_lib.count_bjy_lines testname
-          ; metadata }
-        in
-        row
-      in
-      let trials = List.init n_trials ~f:test_one in
-      let avg_trial =
-        List.fold
-          trials
-          ~init:{
-            testname
-            ; test_result = Concolic.Status.Exhausted_pruned_tree (* just arbitrary initial result *)
-            ; time_to_interpret = Time_float.Span.of_sec 0.0
-            ; time_to_solve = Time_float.Span.of_sec 0.0
-            ; total_time = Time_float.Span.of_sec 0.0
-            ; trial = Average
-            ; lines_of_code = Utils.Cloc_lib.count_bjy_lines testname (* won't even average the remaining fields out. Just pre-calculate it *)
-            ; metadata
-          }
-          ~f:(fun acc x ->
-            { acc with (* sum up *)
-              test_result = x.test_result (* keeps most recent test result *)
-            ; time_to_interpret = Time_float.Span.(acc.time_to_interpret + x.time_to_interpret)
-            ; time_to_solve = Time_float.Span.(acc.time_to_solve + x.time_to_solve)
-            ; total_time = Time_float.Span.(acc.total_time + x.total_time)
-            })
-        |> fun r ->
-          { r with (* average out *)
-            time_to_interpret = Time_float.Span.(r.time_to_interpret / (Int.to_float n_trials))
-          ; time_to_solve = Time_float.Span.(r.time_to_solve / (Int.to_float n_trials))
-          ; total_time = Time_float.Span.(r.total_time / (Int.to_float n_trials))
-          }
-      in
-      trials @ [ avg_trial ]
+      | Number of int
+      | Average
   end
 
-module Result_table =
-  struct
-    type t = Report_row.t Latex_tbl.t
+  type t =
+    { testname          : Filename.t
+    ; test_result       : Concolic.Status.Terminal.t
+    ; time_to_interpret : Time_float.Span.t
+    ; time_to_solve     : Time_float.Span.t
+    ; total_time        : Time_float.Span.t
+    ; trial             : Trial.t
+    ; lines_of_code     : int
+    ; metadata          : Metadata.t }
 
-    let of_dirs ?(avg_only : bool = true)  (n_trials : int) (dirs : Filename.t list) : t =
-      let open List.Let_syntax in
-      { row_module = (module Report_row)
-      ; rows =
-        dirs
-        |> Utils.File_utils.get_all_bjy_files
-        |> List.sort ~compare:(fun a b -> String.compare (Filename.basename a) (Filename.basename b))
-        >>= Report_row.of_testname n_trials
-        |> List.filter ~f:(fun (row : Report_row.t) ->
-          not avg_only || match row.trial with Average -> true | _ -> false
+  let names =
+    [ "Test Name" ; "Interp" ; "Solve" ; "Total" ; "LOC" ]
+    @ (List.map Ttag.all ~f:(fun tag ->
+        Latex_format.rotate_90
+        @@ Ttag.to_string_with_underline tag
+      )
+    )
+
+  let to_strings x =
+    let span_to_ms_string =
+      fun span ->
+        let fl = Time_float.Span.to_ms span in
+        Float.to_string @@
+        if Float.(fl < 1.)
+        then Float.round_decimal fl ~decimal_digits:1
+        else Float.round_significant fl ~significant_digits:2
+    in
+    [ Filename.basename x.testname |> String.take_while ~f:(Char.(<>) '.') |> Latex_format.texttt
+    ; span_to_ms_string x.time_to_interpret
+    ; span_to_ms_string x.time_to_solve
+    ; span_to_ms_string x.total_time
+    ; Int.to_string x.lines_of_code ]
+    @ (
+      match Metadata.tags_of_t x.metadata with
+      | `Sorted_list ls ->
+        List.map ls ~f:(function
+          | `Absent -> "--"
+          | `Feature tag -> Ttag.to_string_short tag
+          | `Reason tag ->
+            Latex_format.red
+            @@ Ttag.to_string_short tag
           )
-        >>| Latex_tbl.Row_or_hline.return
-        |> List.cons Latex_tbl.Row_or_hline.Hline
-      ; columns =
-        let little_space = Latex_tbl.Col_option.Little_space { point_size = 3 } in
-        [ [ Latex_tbl.Col_option.Right_align ; Vertical_line_to_right ]
-        ; [ little_space ] (* interp time *)
-        ; [ little_space ] (* solve time *)
-        ; [ little_space ;  Vertical_line_to_right ] (* total time *)
-        ; [ little_space ; Vertical_line_to_right ] (* loc *) ]
-        @
-        List.init (List.length Ttag.all) ~f:(fun _ -> [ little_space ]) 
-      }
-  end
+    )
+
+  let of_testname (n_trials : int) (testname : Filename.t) : t list =
+    assert (n_trials > 0);
+    let metadata = Metadata.of_bjy_file testname in
+    let test_one (n : int) : t =
+      let interp0 = Utils.Safe_cell.get Concolic.Evaluator.global_runtime in
+      let solve0 = Utils.Safe_cell.get Concolic.Evaluator.global_solvetime in
+      let t0 = Caml_unix.gettimeofday () in
+      let source =
+        In_channel.read_all testname
+        |> Lang.Parse.parse_single_pgm_string
+      in
+      let test_result =
+        Concolic.Driver.test_bjy source ~global_timeout_sec:90.0 ~do_wrap:true ~in_parallel:false (* parallel computation off by default *)
+      in
+      let t1 = Caml_unix.gettimeofday () in
+      let interp1 = Utils.Safe_cell.get Concolic.Evaluator.global_runtime in
+      let solve1 = Utils.Safe_cell.get Concolic.Evaluator.global_solvetime in
+      let row =
+        { testname
+        ; test_result
+        ; time_to_interpret = Time_float.Span.of_sec (interp1 -. interp0)
+        ; time_to_solve = Time_float.Span.of_sec (solve1 -. solve0)
+        ; total_time = Time_float.Span.of_sec (t1 -. t0)
+        ; trial = Number n
+        ; lines_of_code = Utils.Cloc_lib.count_bjy_lines testname
+        ; metadata }
+      in
+      row
+    in
+    let trials = List.init n_trials ~f:test_one in
+    let avg_trial =
+      List.fold
+        trials
+        ~init:{
+          testname
+          ; test_result = Concolic.Status.Exhausted_pruned_tree (* just arbitrary initial result *)
+          ; time_to_interpret = Time_float.Span.of_sec 0.0
+          ; time_to_solve = Time_float.Span.of_sec 0.0
+          ; total_time = Time_float.Span.of_sec 0.0
+          ; trial = Average
+          ; lines_of_code = Utils.Cloc_lib.count_bjy_lines testname (* won't even average the remaining fields out. Just pre-calculate it *)
+          ; metadata
+        }
+        ~f:(fun acc x ->
+          { acc with (* sum up *)
+            test_result = x.test_result (* keeps most recent test result *)
+          ; time_to_interpret = Time_float.Span.(acc.time_to_interpret + x.time_to_interpret)
+          ; time_to_solve = Time_float.Span.(acc.time_to_solve + x.time_to_solve)
+          ; total_time = Time_float.Span.(acc.total_time + x.total_time)
+          })
+      |> fun r ->
+        { r with (* average out *)
+          time_to_interpret = Time_float.Span.(r.time_to_interpret / (Int.to_float n_trials))
+        ; time_to_solve = Time_float.Span.(r.time_to_solve / (Int.to_float n_trials))
+        ; total_time = Time_float.Span.(r.total_time / (Int.to_float n_trials))
+        }
+    in
+    trials @ [ avg_trial ]
+end
+
+module Result_table = struct
+  type t = Report_row.t Latex_tbl.t
+
+  let of_dirs ?(avg_only : bool = true)  (n_trials : int) (dirs : Filename.t list) : t =
+    let open List.Let_syntax in
+    { row_module = (module Report_row)
+    ; rows =
+      dirs
+      |> Utils.File_utils.get_all_bjy_files
+      |> List.sort ~compare:(fun a b -> String.compare (Filename.basename a) (Filename.basename b))
+      >>= Report_row.of_testname n_trials
+      |> List.filter ~f:(fun (row : Report_row.t) ->
+        not avg_only || match row.trial with Average -> true | _ -> false
+        )
+      >>| Latex_tbl.Row_or_hline.return
+      |> List.cons Latex_tbl.Row_or_hline.Hline
+    ; columns =
+      let little_space = Latex_tbl.Col_option.Little_space { point_size = 3 } in
+      [ [ Latex_tbl.Col_option.Right_align ; Vertical_line_to_right ]
+      ; [ little_space ] (* interp time *)
+      ; [ little_space ] (* solve time *)
+      ; [ little_space ;  Vertical_line_to_right ] (* total time *)
+      ; [ little_space ; Vertical_line_to_right ] (* loc *) ]
+      @
+      List.init (List.length Ttag.all) ~f:(fun _ -> [ little_space ]) 
+    }
+end
 
 let run n_trials dirs =
   let oc_null = Out_channel.create "/dev/null" in
