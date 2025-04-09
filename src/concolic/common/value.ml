be1f1e41ffb46497a0922404e1f7a64e1ab7e4af
Brandon Stride <brandonzstride@hotmail.com>
2025-04-09 19:18:15 -0400

    refactor: intermediate concolic value module

--- src/concolic/common/value.ml
+++ src/concolic/common/value.ml
@@ -1,4 +1,9 @@
 
-include Lang.Value.Embedded (struct type 'a t = 'a * 'a Expression.t let to_string f (v, _) = f v end)
+module Concolic_value = struct
+  type 'a t = 'a * 'a Expression.t
+  let to_string f (v, _) = f v
+end
+
+include Lang.Value.Embedded (Concolic_value)
 
 include T
