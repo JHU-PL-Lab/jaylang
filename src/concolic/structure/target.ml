b857ca97eb85a79f8a45e14d5567fbb65b6dc529
Brandon Stride <brandonzstride@hotmail.com>
2025-04-13 10:06:56 -0400

    refactor: delete explicit path tree

--- src/concolic/structure/target.ml
+++ src/concolic/structure/target.ml
@@ -3,20 +3,22 @@
 let uid = Utils.Counter.create ()
 
 type t =
-  { dir    : Direction.Packed.t
-  ; path_n : int
-  ; path   : Path.Reverse.t (* maybe should hashcons this instead of the uniq id hack to be more safe (but hashcons would be slower) *)
+  { path_n : int
   ; uniq_id : int
-  }
+  ; exprs : bool Expression.t list }
 
-let make (rev_path : Path.Reverse.t) : t =
-  match rev_path.backward_path with
-  | [] -> failwith "bad empty target"
-  | dir :: backward_path ->
-    { dir
-    ; path = { backward_path }
-    ; path_n = List.length backward_path + 1
-    ; uniq_id = Utils.Counter.next uid }
+let empty : t =
+  { path_n = 0
+  ; uniq_id = Utils.Counter.next uid
+  ; exprs = [] }
+
+let cons (claim : 'a Claim.t) (target : t) : t =
+  { path_n = target.path_n + 1
+  ; uniq_id = Utils.Counter.next uid
+  ; exprs = Claim.to_expression claim :: target.exprs }
+
+let to_expressions ({ exprs ; _ } : t) : bool Expression.t list =
+  exprs
 
 (*
   SUPER IMPORTANT NOTE:
@@ -26,33 +28,11 @@ let make (rev_path : Path.Reverse.t) : t =
   * This will break if the concolic evaluator does not have this
     property, and it won't break loudly, so the developer must be
     very careful that this assumption continues to hold.
-  * Use the commented comparison if that assumption no longer holds.
+  * If only equality were needed, we would use `a == b`, as it
+    does the same.
 *)
 let compare (a : t) (b : t) : int =
   Int.compare a.uniq_id b.uniq_id
-  (* match Int.compare a.path_n b.path_n with
-  | 0 -> begin
-    match Direction.Packed.compare a.dir b.dir with
-    | 0 -> Path.Reverse.compare a.path b.path
-    | x -> x
-  end 
-  | x -> x *)
-
-let to_rev_path (target : t) : Path.Reverse.t =
-  Path.Reverse.cons target.dir target.path
-
-let to_path (target : t) : Path.t =
-  Path.Reverse.to_forward_path
-  @@ to_rev_path target
-
-let append_path (path_to : Path.t) (target : t) : t =
-  { dir = target.dir
-  ; path_n = target.path_n + List.length path_to.forward_path
-  ; path = Path.Reverse.concat target.path @@ Path.Reverse.of_forward_path path_to
-  ; uniq_id = Utils.Counter.next uid }
 
 let path_n ({ path_n ; _ } : t) : int =
   path_n
-
-let dir ({ dir ; _ } : t) : Direction.Packed.t =
-  dir
