f67b140834bf422c498df8c0c278b5a8262df86b
Brandon Stride <brandonzstride@hotmail.com>
2025-07-03 13:30:26 -0400

    fix: compare callstacks in correct order

--- src/interp-common/callstack.ml
+++ src/interp-common/callstack.ml
@@ -3,11 +3,21 @@ open Core
 
 type t = 
   | Callstack of Lang.Ast.Program_point.t list [@@unboxed]
-  [@@deriving compare, equal]
+
+(* Need to compare back to front because back is oldest *)
+(* Could be smart and not reverse, but then we're using stack space.
+  I'll just reverse them for now. *)
+let compare (Callstack a : t) (Callstack b : t) : int =
+  List.compare Lang.Ast.Program_point.compare (List.rev a) (List.rev b)
+
+let equal (Callstack a : t) (Callstack b : t) : bool =
+  List.equal Lang.Ast.Program_point.equal (List.rev a) (List.rev b)
 
 let to_string : t -> string = function
   | Callstack ls ->
-    String.concat ~sep:"." (List.map ls ~f:(fun (Lang.Ast.Program_point.Program_point i) -> Int.to_string i))
+    List.rev ls
+    |> List.map ~f:(fun (Lang.Ast.Program_point.Program_point i) -> Int.to_string i)
+    |> String.concat ~sep:"."
 
 let empty = Callstack []
 
