a8ea578334c889877f995b47c74d2c2498df46c2
Brandon Stride <brandonzstride@hotmail.com>
2025-07-23 16:08:13 -0700

    refactor: module typing in concolic solver

--- src/concolic/solve/solver.ml
+++ src/concolic/solve/solver.ml
@@ -3,8 +3,8 @@ module type S = sig
   val solve : bool Formula.t list -> Formula.Key.t Overlays.Typed_smt.solution
 end
 
-module Make () = Overlays.Typed_smt.Solve (Overlays.Typed_smt.Make_Z3 (struct let ctx = Z3.mk_context [] end))
+module Make () : S = Overlays.Typed_smt.Solve (Overlays.Typed_smt.Make_Z3 (struct let ctx = Z3.mk_context [] end))
 
-module Default = Make ()
+module Default : S = Make ()
 
 include Default
---
+++ src/eager-concolic/eval.ml
@@ -0,0 +1,5 @@
+
+(*
+  Put eager concolic eval here. 
+  Then the concolic loop can call out to this.
+*)
